688090fbc21e192fe9a03482691b7210bdfb826a
Tuomas Karna <tuomas.karna@gmail.com>
2015-07-07 13:10:45 +0200

    removed obsolete tracerAdvection example

--- examples/tracerAdvection/regeneratePVD.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-end=540
-OUT=$1
-python generatePVD.py -d $OUT -e $end -n Velocity3d
-python generatePVD.py -d $OUT -e $end -n VertVelo3d
-python generatePVD.py -d $OUT -e $end -n Salinity3d
-
