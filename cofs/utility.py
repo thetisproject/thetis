bffe244c8418f58ea727b3d34a94d8663a6eda47
Tuomas Karna <tuomas.karna@gmail.com>
2015-07-08 19:26:25 +0100

    added equation of state (Jackett et al., 2006)

--- cofs/utility.py
+++ cofs/utility.py
@@ -876,3 +876,52 @@ class projector(object):
 
     def project(self):
         self.solver.solve()
+
+
+class EquationOfState(object):
+    """
+    Equation of State according to [1] for computing sea water density.
+
+    [1] Jackett, D. R., McDougall, T. J., Feistel, R., Wright, D. G., and
+        Griffies, S. M. (2006). Algorithms for Density, Potential Temperature,
+        Conservative Temperature, and the Freezing Temperature of Seawater.
+        Journal of Atmospheric and Oceanic Technology, 23(12):1709–1728.
+    """
+    def __init__(self):
+        # polynomial coefficients
+        self.a = np.array([9.9984085444849347e2, 7.3471625860981584e0, -5.3211231792841769e-2,
+                           3.6492439109814549e-4, 2.5880571023991390e0, -6.7168282786692355e-3,
+                           1.9203202055760151e-3, 1.1798263740430364e-2, 9.8920219266399117e-8,
+                           4.6996642771754730e-6, -2.5862187075154352e-8, -3.2921414007960662e-12])
+        self.b = np.array([1.0, 7.2815210113327091e-3, -4.4787265461983921e-5, 3.3851002965802430e-7,
+                           1.3651202389758572e-10, 1.7632126669040377e-3, -8.8066583251206474e-6,
+                           -1.8832689434804897e-10, 5.7463776745432097e-6, 1.4716275472242334e-9,
+                           6.7103246285651894e-6, -2.4461698007024582e-17, -9.1534417604289062e-18])
+
+    def computeRho(self, S, Th, p, rho0=0.0):
+        """
+        Computes sea water density.
+
+        :param S: Salinity expressed on the Practical Salinity Scale 1978
+        :param Th: Potential temperature in Celsius
+        :param p: Pressure in decibars (1 dbar = 1e4 Pa)
+        :param rho0: Optional reference density
+
+        Th is referenced to pressure p_r = 0 dbar. All pressures are gauge
+        pressures: they are the absolute pressures minus standard atmosperic
+        pressure 10.1325 dbar.
+        Last optional argument rho0 is for computing deviation
+        rho' = rho(S, Th, p) - rho0.
+        """
+        S0 = np.maximum(S, 0.0)  # ensure positive salinity
+        a = self.a
+        b = self.b
+        Pn = (a[0] + Th*a[1] + Th*Th*a[2] + Th*Th*Th*a[3] + S0*a[4] +
+              Th*S0*a[5] + S0*S0*a[6] + p*a[7] + p*Th * Th*a[8] + p*S0*a[9] +
+              p*p*a[10] + p*p*Th*Th * a[11])
+        Pd = (b[0] + Th*b[1] + Th*Th*b[2] + Th*Th*Th*b[3] +
+              Th*Th*Th*Th*b[4] + S0*b[5] + S0*Th*b[6] + S0*Th*Th*Th*b[7] +
+              pow(S0, 1.5)*b[8] + pow(S0, 1.5)*Th*Th*b[9] + p*b[10] +
+              p*p*Th*Th*Th*b[11] + p*p*p*Th*b[12])
+        rho = Pn/Pd - rho0
+        return rho
