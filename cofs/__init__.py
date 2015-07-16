cfe37d6a54c2c14d619e491488e337a51a36cde0
Stephan Kramer <s.kramer@imperial.ac.uk>
2015-07-16 16:27:59 +0100

    timeIntegration module has been renamed timeIntegrator

--- cofs/__init__.py
+++ cofs/__init__.py
@@ -1,7 +1,7 @@
 from utility import *
 import module_2d
 import module_3d
-import timeIntegration
+import timeIntegrator
 import solver
 
 op2.init(log_level=WARNING)  # DEBUG, INFO, WARNING, ERROR, CRITICAL
