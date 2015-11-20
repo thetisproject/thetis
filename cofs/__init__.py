a398934fec94529e6cbefebab04922a89fa20108
Tuomas Karna <tuomas.karna@gmail.com>
2015-11-19 18:01:14 -0800

    disable COFFEE optimizations for now

--- cofs/__init__.py
+++ cofs/__init__.py
@@ -3,5 +3,7 @@ import timeIntegrator
 import solver
 import solver2d
 
+parameters['coffee']={}  # omit COFFEE optimzations for now
+
 op2.init(log_level=WARNING)  # DEBUG, INFO, WARNING, ERROR, CRITICAL
 parameters['pyop2_options']['profiling'] = True
