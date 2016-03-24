dd3f1cc49d4d7fb07fc0a31f2f2fe90a8329aa51
Tuomas Karna <tuomas.karna@gmail.com>
2016-03-24 12:03:08 -0700

    activate coffee optimizations

--- thetis/__init__.py
+++ thetis/__init__.py
@@ -3,7 +3,5 @@ import timeintegrator  # NOQA
 import solver  # NOQA
 import solver2d  # NOQA
 
-parameters['coffee'] = {}  # omit COFFEE optimzations for now
-
 op2.init(log_level=WARNING)  # DEBUG, INFO, WARNING, ERROR, CRITICAL
 parameters['pyop2_options']['profiling'] = True
