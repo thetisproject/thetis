705aba0ce3123f541df4848d477d42ea5e699912
Tuomas Karna <tuomas.karna@gmail.com>
2016-04-11 13:34:39 -0700

    disable assebly cache
    - no need to cache as solvers are created outside time loop
    - helps to keep memory consumption moderate (still some increase)

--- thetis/__init__.py
+++ thetis/__init__.py
@@ -5,3 +5,4 @@ import solver2d  # NOQA
 
 op2.init(log_level=WARNING)  # DEBUG, INFO, WARNING, ERROR, CRITICAL
 parameters['pyop2_options']['profiling'] = True
+parameters['assembly_cache']['enabled'] = False
