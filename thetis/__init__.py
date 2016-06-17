64acf29d0b2f5ef6bcb421fa125cc5930d20266f
Tuomas Karna <tuomas.karna@gmail.com>
2016-06-17 13:26:56 -0700

    enable coffee and matnest
    reverts an earlier erroneous commit

--- thetis/__init__.py
+++ thetis/__init__.py
@@ -5,5 +5,3 @@ import solver2d  # NOQA
 
 op2.init(log_level=WARNING)  # DEBUG, INFO, WARNING, ERROR, CRITICAL
 parameters['assembly_cache']['enabled'] = False
-parameters['coffee'] = {}
-parameters['matnest'] = False
