dfa6ba621af2bc4c2f1a7689b67dc2b7137bfceb
Lawrence Mitchell <lawrence.mitchell@imperial.ac.uk>
2016-07-26 11:44:13 +0100

    init: Remove log_level setting in OP2
    Firedrake now defaults to WARNING across all components.

--- thetis/__init__.py
+++ thetis/__init__.py
@@ -7,5 +7,4 @@ import thetis.solver2d as solver2d  # NOQA
 from thetis.callback import DiagnosticCallback  # NOQA
 import thetis.limiter as limiter      # NOQA
 
-op2.init(log_level=WARNING)
 parameters['assembly_cache']['enabled'] = False
