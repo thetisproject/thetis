51524f095adbd0e82e220e7beb69d38db9630019
Lawrence Mitchell <lawrence.mitchell@imperial.ac.uk>
2016-07-26 11:43:25 +0100

    init: Use explicit module imports
    Fixes #64.

--- thetis/__init__.py
+++ thetis/__init__.py
@@ -1,9 +1,11 @@
-from utility import *
-import timeintegrator  # NOQA
-import solver  # NOQA
-import solver2d  # NOQA
-from callback import DiagnosticCallback  # NOQA
-from log import *
+from __future__ import absolute_import
+from thetis.utility import *
+from thetis.log import *
+import thetis.timeintegrator as timeintegrator
+import thetis.solver as solver  # NOQA
+import thetis.solver2d as solver2d  # NOQA
+from thetis.callback import DiagnosticCallback  # NOQA
+import thetis.limiter as limiter      # NOQA
 
 op2.init(log_level=WARNING)
 parameters['assembly_cache']['enabled'] = False
