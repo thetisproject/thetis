a3b9a98265c56f2a687e618ca1851f3a70ead34c
Stephan Kramer <s.kramer@imperial.ac.uk>
2016-11-08 11:28:52 +0000

    Remove no longer existing assembly cache option.

--- thetis/__init__.py
+++ thetis/__init__.py
@@ -1,10 +1,8 @@
 from __future__ import absolute_import
-from thetis.utility import *
-from thetis.log import *
+from thetis.utility import * # NOQA
+from thetis.log import * # NOQA
 import thetis.timeintegrator as timeintegrator  # NOQA
 import thetis.solver as solver  # NOQA
 import thetis.solver2d as solver2d  # NOQA
 from thetis.callback import DiagnosticCallback  # NOQA
 import thetis.limiter as limiter      # NOQA
-
-parameters['assembly_cache']['enabled'] = False
