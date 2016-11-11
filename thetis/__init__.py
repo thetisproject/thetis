7efc0dec92838d16f4d9bbe764494cfc0eeefe3d
Tuomas Karna <tuomas.karna@gmail.com>
2016-11-10 17:06:49 -0800

    turn off lazy evaluation

--- thetis/__init__.py
+++ thetis/__init__.py
@@ -1,8 +1,10 @@
 from __future__ import absolute_import
-from thetis.utility import * # NOQA
-from thetis.log import * # NOQA
+from thetis.utility import *
+from thetis.log import *
 import thetis.timeintegrator as timeintegrator  # NOQA
 import thetis.solver as solver  # NOQA
 import thetis.solver2d as solver2d  # NOQA
 from thetis.callback import DiagnosticCallback  # NOQA
 import thetis.limiter as limiter      # NOQA
+
+parameters['pyop2_options']['lazy_evaluation'] = False
