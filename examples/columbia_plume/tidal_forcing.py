26fa04ecdec2cce4db88800d52aa7f4bcd849be4
Tuomas Karna <tuomas.karna@gmail.com>
2017-04-14 12:27:17 -0700

    cre-plume: fix parallelization of tidal forcing

--- examples/columbia_plume/tidal_forcing.py
+++ examples/columbia_plume/tidal_forcing.py
@@ -12,6 +12,8 @@ assert os.path.exists(tide_file), msg
 
 def to_latlon(x, y):
     lon, lat = coordsys_spcs.spcs2lonlat(x, y)
+    if lon < 0.0:
+        lon += 360.
     return lat, lon
 
 
@@ -20,27 +22,50 @@ class TidalBoundaryForcing(object):
                  constituents=None, boundary_ids=None):
         if constituents is None:
             constituents = ['Q1', 'O1', 'P1', 'K1', 'N2', 'M2', 'S2', 'K2']
-        tide = uptide.Tides(constituents)
-        tide.set_initial_time(init_time)
-        self.tnci = uptide.tidal_netcdf.FESTidalInterpolator(tide, tide_file, ranges=None)
-        self.tidal_field = tidal_field
+
+        # determine nodes at the boundary
         fs = tidal_field.function_space()
         if boundary_ids is None:
-            # interpolate for the whole domain
+            # interpolate in the whole domain
             self.nodes = np.arange(self.tidal_field.dat.data.shape[0])
         else:
-            self.bc = DirichletBC(fs, 0., boundary_ids)
-            self.nodes = self.bc.nodes
+            bc = DirichletBC(fs, 0., boundary_ids, method='geometric')
+            node_set = bc.node_set
+            tmp_func = fs.get_work_function(zero=True)
+            tmp_func.assign(1, subset=node_set)
+            self.nodes = np.where(tmp_func.dat.data > 0.5)[0]
+            fs.restore_work_function(tmp_func)
 
         xy = SpatialCoordinate(fs.mesh())
         fsx = Function(fs).interpolate(xy[0]).dat.data
         fsy = Function(fs).interpolate(xy[1]).dat.data
+        # compute lat lon bounds for each process
+        if len(self.nodes) > 0:
+            bounds_x = [fsx[self.nodes].min(), fsx[self.nodes].max()]
+            bounds_y = [fsy[self.nodes].min(), fsy[self.nodes].max()]
+            bounds_lat = [1e20, -1e20]
+            bounds_lon = [1e20, -1e20]
+            for x in bounds_x:
+                for y in bounds_y:
+                    lat, lon = to_latlon(x, y)
+                    bounds_lat[0] = min(bounds_lat[0], lat)
+                    bounds_lat[1] = max(bounds_lat[1], lat)
+                    bounds_lon[0] = min(bounds_lon[0], lon)
+                    bounds_lon[1] = max(bounds_lon[1], lon)
+
+            ranges = (bounds_lat, bounds_lon)
+        else:
+            ranges = None
+
+        tide = uptide.Tides(constituents)
+        tide.set_initial_time(init_time)
+        self.tnci = uptide.tidal_netcdf.FESTidalInterpolator(tide, tide_file, ranges=ranges)
+        self.tidal_field = tidal_field
+
         latlon = []
         for node in self.nodes:
             x, y = fsx[node], fsy[node]
             lat, lon = to_latlon(x, y)
-            if lon < 0.0:
-                lon += 360.
             latlon.append((lat, lon))
         self.nll = zip(self.nodes, latlon)
 
@@ -58,15 +83,18 @@ class TidalBoundaryForcing(object):
 # y = 295000.
 # print x, y, to_latlon(x, y)
 
+# import datetime
 # mesh2d = Mesh('mesh_cre-plume002.msh')
 # p1 = FunctionSpace(mesh2d, 'CG', 1)
 # m2_field = Function(p1, name='elevation')
 #
-# tbnd = TidalBoundaryForcing(tnci, m2_field)
+# tbnd = TidalBoundaryForcing(m2_field, datetime.datetime(2016, 5, 1), constituents=['M2'], boundary_ids=[1, 2, 3])
 # tbnd.set_tidal_field(0.0)
 #
-# out = File('tidal_elev.pvd')
+# out = File('tmp/tidal_elev.pvd')
 # for t in np.linspace(0, 12*3600., 60):
 #     tbnd.set_tidal_field(t)
+#     n = norm(m2_field)
+#     if m2_field.function_space().mesh().comm.rank == 0:
+#         print n
 #     out.write(m2_field)
-#
