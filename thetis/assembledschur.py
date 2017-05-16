b943173d8fed119496aad1f94fc4c4d7bf4cdd66
Stephan Kramer <s.kramer@imperial.ac.uk>
2017-05-16 15:17:11 +0100

    Fix memleak and make less verbose.

--- thetis/assembledschur.py
+++ thetis/assembledschur.py
@@ -12,7 +12,6 @@ class AssembledSchurPC(PCBase):
         v and u are the test and trial of the 00 block. This gives the exact
         inverse of the mass matrix for a DG discretisation."""
     def initialize(self, pc):
-        print "Initializing AssembledSchurPC"
         _, P = pc.getOperators()
         ctx = P.getPythonContext()
         a = ctx.appctx['a']
@@ -24,40 +23,42 @@ class AssembledSchurPC(PCBase):
         v = TestFunction(V)
         u = TrialFunction(V)
         mass = dot(v, u)*dx
-        self.A00 = assemble(mass).M.handle
-        self.A00_inv = assemble(mass, inverse=True).M.handle
-        self.A00_inv.convert(PETSc.Mat.Type.AIJ)
+        self.A00_inv = assemble(mass, inverse=True, mat_type='aij').M.handle
         self.A10_A00_inv = None
         self.schur = None
         self.schur_plus = None
 
         fs = dict(formmanipulation.split_form(a))
-        self.a01 = fs[(0, 1)]
-        self.a10 = fs[(1, 0)]
-        self.a11 = fs[(1, 1)]
+        self.a01 = fs[(0,1)]
+        self.a10 = fs[(1,0)]
+        self.a11 = fs[(1,1)]
+        self.A01 = None
+        self.A10 = None
+        self.A11 = None
         self.ksp = PETSc.KSP().create()
         self.ksp.setOptionsPrefix(options_prefix + 'schur_')
         self.ksp.setFromOptions()
         self.update(pc)
 
     def update(self, pc):
-        print "Updating AssembledSchurPC"
-        self.A01 = assemble(self.a01).M.handle
-        self.A10 = assemble(self.a10).M.handle
-        self.A11 = assemble(self.a11).M.handle
+        self.A01 = assemble(self.a01, tensor=self.A01)
+        self.A10 = assemble(self.a10, tensor=self.A10)
+        self.A11 = assemble(self.a11, tensor=self.A11)
+        A01 = self.A01.M.handle
+        A10 = self.A10.M.handle
+        A11 = self.A11.M.handle
 
-        self.A10_A00_inv = self.A10.matMult(self.A00_inv, self.A10_A00_inv, 2.0)
-        self.schur = self.A10_A00_inv.matMult(self.A01, self.schur, 2.0)
+        self.A10_A00_inv = A10.matMult(self.A00_inv, self.A10_A00_inv, 2.0)
+        self.schur = self.A10_A00_inv.matMult(A01, self.schur, 2.0)
         if self.schur_plus is None:
-            self.schur_plus = self.schur.duplicate(copy=True)
+          self.schur_plus = self.schur.duplicate(copy=True)
+          self.schur_plus.aypx(-1.0, A11, PETSc.Mat.Structure.DIFFERENT_NONZERO_PATTERN)
         else:
-            self.schur_plus = self.schur.copy(self.schur_plus, PETSc.Mat.Structure.DIFFERENT_NONZERO_PATTERN)
-        self.schur_plus.aypx(-1.0, self.A11, PETSc.Mat.Structure.DIFFERENT_NONZERO_PATTERN)
+          self.schur_plus = self.schur.copy(self.schur_plus, PETSc.Mat.Structure.DIFFERENT_NONZERO_PATTERN)
+          self.schur_plus.aypx(-1.0, A11, PETSc.Mat.Structure.SAME_NONZERO_PATTERN)
         self.ksp.setOperators(self.schur_plus)
 
     def apply(self, pc, X, Y):
-        print "Applying AssembledSchurPC"
-
         self.ksp.solve(X, Y)
         r = self.ksp.getConvergedReason()
         if r < 0:
