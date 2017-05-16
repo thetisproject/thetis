c6e1b8d5beda456a667bdca16c819ea5ae7cb109
Stephan Kramer <s.kramer@imperial.ac.uk>
2017-05-16 18:52:46 +0100

    Use assembledschur pc in test.

--- test/swe2d/test_standing_wave.py
+++ test/swe2d/test_standing_wave.py
@@ -44,15 +44,54 @@ def test_standing_wave_channel(timesteps, max_rel_err, timestepper, do_export=Fa
     solver_obj.options.simulation_export_time = dt
     solver_obj.options.simulation_end_time = t_end
     solver_obj.options.no_exports = not do_export
-    solver_obj.options.element_family = 'dg-dg'
     solver_obj.options.timestepper_type = timestepper
-    if timestepper == 'CrankNicolson':
-        # when linearising the equations, CrankNicolson (theta=0.5) stops being 2nd order
-        # (for PressureProjectionPicard we restore 2nd order because of the 2 iterations)
-        solver_obj.options.timestepper_options.use_semi_implicit_linearization = False
+
+    if timestepper == 'pressureprojectionpicard':
+        # this approach currently only works well with dg-cg, because in dg-dg
+        # the pressure gradient term puts an additional stabilisation term in the velocity block
+        # (even without that term  this approach is not as fast, as the stencil for the assembled schur system
+        # is a lot bigger for dg-dg than dg-cg)
+        solver_obj.options.element_family = 'dg-cg'
+        solver_obj.options.use_linearized_semi_implicit_2d = True
+        # solver options for the linearized wave equation terms
+        solver_obj.options.solver_parameters_sw = {
+            'snes_type': 'ksponly',  # we've linearized, so no snes needed
+            'ksp_type': 'preonly',  # we solve the full schur complement exactly, so no need for outer krylov
+            'mat_type': 'matfree',
+            'pc_type': 'fieldsplit',
+            'pc_fieldsplit_type': 'schur',
+            'pc_fieldsplit_schur_fact_type': 'full',
+            # velocity mass block:
+            'fieldsplit_U_2d_ksp_type': 'gmres',
+            'fieldsplit_U_2d_pc_type': 'python',
+            'fieldsplit_U_2d_pc_python_type': 'firedrake.AssembledPC',
+            'fieldsplit_U_2d_assembled_ksp_type': 'preonly',
+            'fieldsplit_U_2d_assembled_pc_type': 'bjacobi',
+            'fieldsplit_U_2d_assembled_sub_pc_type': 'ilu',
+            # schur system: we tell it to explicitly assemble the schur system
+            # which only works with pressureprojectionicard where the velocity block is just the mass matrix
+            # and if the velocity is DG so that this mass matrix can be inverted explicitly
+            'fieldsplit_1_ksp_type': 'preonly',
+            'fieldsplit_1_pc_type': 'python',
+            'fieldsplit_1_pc_python_type': 'thetis.AssembledSchurPC',
+            # options to solve the assembled schur system
+            'fieldsplit_1_schur_ksp_type': 'preonly',
+            'fieldsplit_1_schur_ksp_max_it': 100,
+            'fieldsplit_1_schur_ksp_converged_reason': True,
+            'fieldsplit_1_schur_pc_type': 'gamg',
+        }
+        solver_obj.options.solver_parameters_sw_momentum = {
+            'snes_monitor': True,
+            'snes_type': 'ksponly',
+            'ksp_type': 'gmres',
+            'ksp_converged_reason': True,
+            'pc_type': 'bjacobi',
+            'pc_bjacobi_type': 'ilu',
+        }
     if hasattr(solver_obj.options.timestepper_options, 'use_automatic_timestep'):
         solver_obj.options.timestepper_options.use_automatic_timestep = False
     solver_obj.options.timestep = dt
+    solver_obj.options.shallow_water_theta = 0.5
 
     # boundary conditions
     solver_obj.bnd_functions['shallow_water'] = {}
