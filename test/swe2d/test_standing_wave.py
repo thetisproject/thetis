98b4640923852a6932af3de868c68814a69a78d1
Stephan Kramer <s.kramer@imperial.ac.uk>
2017-05-16 18:52:46 +0100

    Use assembledschur pc in test.

--- test/swe2d/test_standing_wave.py
+++ test/swe2d/test_standing_wave.py
@@ -44,10 +44,10 @@ def test_steady_state_channel(timesteps, max_rel_err, timestepper, do_export=Fal
     solver_obj.options.t_export = dt
     solver_obj.options.t_end = t_end
     solver_obj.options.no_exports = not do_export
-    solver_obj.options.element_family = 'dg-dg'
     solver_obj.options.timestepper_type = timestepper
     solver_obj.options.shallow_water_theta = 0.5
     if timestepper == 'cranknicolson':
+        solver_obj.options.element_family = 'dg-dg'
         solver_obj.options.solver_parameters_sw = {
             'ksp_type': 'preonly',
             'pc_type': 'lu',
@@ -56,25 +56,42 @@ def test_steady_state_channel(timesteps, max_rel_err, timestepper, do_export=Fal
             'snes_type': 'newtonls',
         }
     elif timestepper == 'pressureprojectionpicard':
+        # this approach currently only works well with dg-cg, because in dg-dg
+        # the pressure gradient term puts an additional stabilisation term in the velocity block
+        # (even without that term  this approach is not as fast, as the stencil for the assembled schur system
+        # is a lot bigger for dg-dg than dg-cg)
+        solver_obj.options.element_family = 'dg-cg'
+        solver_obj.options.use_linearized_semi_implicit_2d = True
         # solver options for the linearized wave equation terms
         solver_obj.options.solver_parameters_sw = {
             'snes_type': 'ksponly',  # we've linearized, so no snes needed
             'ksp_type': 'preonly',  # we solve the full schur complement exactly, so no need for outer krylov
+            'mat_type': 'matfree',
             'pc_type': 'fieldsplit',
             'pc_fieldsplit_type': 'schur',
             'pc_fieldsplit_schur_fact_type': 'full',
-            'pc_fieldsplit_schur_precondition': 'selfp',
             # velocity mass block:
-            'fieldsplit_0_ksp_type': 'preonly',  # NOTE: this is only an exact solver for the velocity mass block if velocity is DG
-            'fieldsplit_0_pc_type': 'ilu',
-            'fieldsplit_1_ksp_type': 'gmres',
-            # schur complement:
-            'fieldsplit_1_pc_type': 'gamg',
-            'fieldsplit_1_ksp_max_it': 100,
-            'fieldsplit_1_ksp_converged_reason': True,
+            'fieldsplit_U_2d_ksp_type': 'gmres',
+            'fieldsplit_U_2d_pc_type': 'python',
+            'fieldsplit_U_2d_pc_python_type': 'firedrake.AssembledPC',
+            'fieldsplit_U_2d_assembled_ksp_type': 'preonly',
+            'fieldsplit_U_2d_assembled_pc_type': 'bjacobi',
+            'fieldsplit_U_2d_assembled_sub_pc_type': 'ilu',
+            # schur system: we tell it to explicitly assemble the schur system
+            # which only works with pressureprojectionicard where the velocity block is just the mass matrix
+            # and if the velocity is DG so that this mass matrix can be inverted explicitly
+            'fieldsplit_1_ksp_type': 'preonly',
+            'fieldsplit_1_pc_type': 'python',
+            'fieldsplit_1_pc_python_type': 'thetis.AssembledSchurPC',
+            # options to solve the assembled schur system
+            'fieldsplit_1_schur_ksp_type': 'preonly',
+            'fieldsplit_1_schur_ksp_max_it': 100,
+            'fieldsplit_1_schur_ksp_converged_reason': True,
+            'fieldsplit_1_schur_pc_type': 'gamg',
         }
-        options.solver_parameters_sw_momentum = {
+        solver_obj.options.solver_parameters_sw_momentum = {
             'snes_monitor': True,
+            'snes_type': 'ksponly',
             'ksp_type': 'gmres',
             'ksp_converged_reason': True,
             'pc_type': 'bjacobi',
