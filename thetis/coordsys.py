2adbb727309437b332d8648c1605e91a4a591275
Tuomas Karna <tuomas.karna@gmail.com>
2017-08-03 15:48:26 -0700

    update coordsys.py
    Better function names, docstrings etc.

--- thetis/coordsys.py
+++ thetis/coordsys.py
@@ -6,9 +6,6 @@ Uses pyproj library.
 import pyproj
 import numpy as np
 
-# NOTE this adds pyproj as dependencency
-# TODO generalize; define unique names for commonly-used systems
-
 
 UTM_ZONE10 = pyproj.Proj(
     proj='utm',
@@ -18,47 +15,56 @@ UTM_ZONE10 = pyproj.Proj(
     errcheck=True)
 SPCS_N_OR = pyproj.Proj(init='nad27:3601', errcheck=True)
 LL_WGS84 = pyproj.Proj(proj='latlong', datum='WGS84', errcheck=True)
-LL_WO = pyproj.Proj(proj='latlong', nadgrids='WO', errcheck=True)  # HARN
 
 
-def convertCoords(x, y, fromSys, toSys):
-    """Converts x,y from fromSys to toSys."""
+def convert_coords(source_sys, target_sys, x, y):
+    """
+    Converts coordinates from source_sys to target_sys
+
+    This function extends pyproj.transform method by handling NaNs correctly.
+
+    :arg source_sys: pyproj coordinate system where (x, y) are defined in
+    :arg target_sys: target pyproj coordinate system
+    :arg x: x coordinate
+    :arg y: y coordinate
+    :type x: float or numpy.array_like
+    :type y: float or numpy.array_like
+    """
     if isinstance(x, np.ndarray):
         # proj may give wrong results if nans in the arrays
-        lon = np.ones_like(x) * np.nan
-        lat = np.ones_like(y) * np.nan
+        lon = np.full_like(x, np.nan)
+        lat = np.full_like(y, np.nan)
         goodIx = np.logical_and(np.isfinite(x), np.isfinite(y))
         lon[goodIx], lat[goodIx] = pyproj.transform(
-            fromSys, toSys, x[goodIx], y[goodIx])
+            source_sys, target_sys, x[goodIx], y[goodIx])
     else:
-        lon, lat = pyproj.transform(fromSys, toSys, x, y)
+        lon, lat = pyproj.transform(source_sys, target_sys, x, y)
     return lon, lat
 
 
-def getVectorRotationMatrix(lon, lat, target_csys, source_csys=None,
-                            delta=None):
+def get_vector_rotation_matrix(source_sys, target_sys, x, y, delta=None):
     """
-    Estimate rotation matrix that converts vectors from source_csys to
-    target_csys at (lon, lat) location.
+    Estimate rotation matrix that converts vectors defined in source_sys to
+    target_sys.
 
-
-    A vector can defined at (lon, lat) can then be transformed as
+    Assume that we have a vector field defined in source_sys: vectors located at
+    (x, y) define the x and y components. We can then rotate the vectors to
+    represent x2 and y2 components of the target_sys by applying a local
+    rotation:
 
     .. code-block:: python
 
-        R, theta = getVectorRotationMatrix(lon, lat, target_csys)
-        v_ll = numpy.array([[v_lon], [v_lat]])
-        v_csys = numpy.matmul(R, v_ll)
-        v_x, v_y = v_csys
+        R, theta = get_vector_rotation_matrix(source_sys, target_sys, x, lat)
+        v_xy = numpy.array([[v_x], [v_y]])
+        v_new = numpy.matmul(R, v_xy)
+        v_x2, v_y2 = v_new
 
     """
-    if source_csys is None:
-        source_csys = LL_WO
     if delta is None:
         delta = 1e-6  # ~1 m in LL_WO
-    x, y = pyproj.transform(source_csys, target_csys, lon, lat)
+    x, y = pyproj.transform(source_sys, target_sys, x, y)
 
-    x2, y2 = pyproj.transform(source_csys, target_csys, lon, lat + delta)
+    x2, y2 = pyproj.transform(source_sys, target_sys, x, y + delta)
     dxdl = (x2 - x) / delta
     dydl = (y2 - y) / delta
     theta = np.arctan2(-dxdl, dydl)
@@ -72,26 +78,28 @@ def getVectorRotationMatrix(lon, lat, target_csys, source_csys=None,
 
 class VectorCoordSysRotation(object):
     """
-    Rotates vectors defined in (lat,lon) to desired local coordinates
-    """
-    def __init__(self, lon, lat, target_csys):
-        """
-        :arg lon: lon coordinates of mesh points
-        :arg lat: lat coordinates of mesh points
-        :arg target_csys: target pyproj coordinate system object
-        """
-        self.target_csys = target_csys
-        self._compute_rotation(lon, lat)
+    Rotates vectors defined in source_sys coordinates to a different coordinate
+    system.
 
-    def _compute_rotation(self, lon, lat):
-        R, theta = getVectorRotationMatrix(lon, lat, self.target_csys)
+    """
+    def __init__(self, source_sys, target_sys, x, y):
+        """
+        :arg source_sys: pyproj coordinate system where (x, y) are defined in
+        :arg target_sys: target pyproj coordinate system
+        :arg x: xgitude coordinate
+        :arg y: yyitude coordinate
+        """
+        self.target_sys = target_sys
+        R, theta = get_vector_rotation_matrix(x, y, self.target_sys)
         self.rotation_sin = np.sin(theta)
         self.rotation_cos = np.cos(theta)
 
-    def __call__(self, v_lon, v_lat):
-        """Apply rotation to vectors with components v_lat, v_lon"""
-        # | c -s | | v_lon |
-        # | s  c | | v_lat |
-        u = v_lon * self.rotation_cos - v_lat * self.rotation_sin
-        v = v_lon * self.rotation_sin + v_lat * self.rotation_cos
+    def __call__(self, v_x, v_y):
+        """
+        Rotate vectors defined by the `v_x` and `v_y` components
+        """
+        # | c -s | | v_x |
+        # | s  c | | v_y |
+        u = v_x * self.rotation_cos - v_y * self.rotation_sin
+        v = v_x * self.rotation_sin + v_y * self.rotation_cos
         return u, v
