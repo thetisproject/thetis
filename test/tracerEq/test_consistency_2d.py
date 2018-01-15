6279391119c74513331831d373ae74f782d4ad63
Athanasios Angeloudis <a.angeloudis06@imperial.ac.uk>
2018-01-15 18:19:05 +0000

    test_consistency_2d test

---
+++ test/tracerEq/test_consistency_2d.py
@@ -0,0 +1,161 @@
+# Tracer box in 3D
+# ================
+#
+# Solves a standing wave in a rectangular basin using wave equation.
+#
+# This version uses a constant tracer to check local/global conservation of tracers.
+#
+# Initial condition for elevation corresponds to a standing wave.
+# Time step and export interval are chosen based on theorethical
+# oscillation frequency. Initial condition repeats every 20 exports.
+#
+#
+# Tuomas Karna 2015-03-11
+from thetis import *
+import pytest
+
+
+def run_tracer_consistency(constant_c = True, **model_options):
+    # meshtype = model_options.pop('meshtype')
+
+    t_cycle = 2000.0  # standing wave period
+    depth = 50.0
+    lx = np.sqrt(9.81*depth)*t_cycle  # wave length
+    ly = 3000.0
+    nx = 18
+    ny = 2
+    mesh2d = RectangleMesh(nx, ny, lx, ly)
+    tracer_value = 4.5
+    elev_amp = 2.0
+    # estimate of max advective velocity used to estimate time step
+    u_mag = Constant(1.0)
+
+    # sloped = False
+    # warped = False
+    #
+    # if meshtype == 'sloped':
+    #     sloped = True
+    # elif meshtype == 'warped':
+    #     warped = True
+    #
+    # suffix = ''
+    # if sloped:
+    #     suffix = '_sloped'
+    # if warped:
+    #     suffix = '_warped'
+    outputdir = 'outputs' # + suffix
+
+    # bathymetry
+    p1_2d = FunctionSpace(mesh2d, 'CG', 1)
+    bathymetry_2d = Function(p1_2d, name='Bathymetry')
+    bathymetry_2d.assign(depth)
+    x_2d, y_2d = SpatialCoordinate(mesh2d)
+    # if sloped:
+    #     bathymetry_2d.interpolate(depth + 20.0*x_2d/lx)
+
+    # set time step, export interval and run duration
+    n_steps = 8
+    t_export = round(float(t_cycle/n_steps))
+    t_end = 2*t_cycle
+
+    # create solver
+    solver_obj = solver2d.FlowSolver2d(mesh2d, bathymetry_2d)
+
+    # if warped:
+    #     # warp interior mesh, top/bottom surfaces are horizontal
+    #     coords = solver_obj.mesh.coordinates
+    #     z = coords.dat.data[:, 2].copy()
+    #     x = coords.dat.data[:, 0]
+    #     p = 2.5*x/lx + 0.5
+    #     sigma = -depth * (0.5*np.tanh(p*(-2.0*z/depth - 1.0))/np.tanh(p) + 0.5)
+    #     coords.dat.data[:, 2] = sigma
+
+    options = solver_obj.options
+    options.use_nonlinear_equations = True
+    options.solve_tracer = True
+    options.use_limiter_for_tracers = True
+    options.simulation_export_time = t_export
+    options.simulation_end_time = t_end
+    options.horizontal_velocity_scale = Constant(u_mag)
+    options.check_volume_conservation_2d = True
+    options.check_tracer_conservation_2d = True
+    options.timestepper_type = 'CrankNicolson'
+    options.output_directory = outputdir
+    options.fields_to_export = ['uv_2d', 'elev_2d', 'tracer_2d']
+    # options.update(model_options)
+    if not options.no_exports:
+        print_output('Exporting to {:}'.format(options.output_directory))
+
+    solver_obj.create_function_spaces()
+    elev_init = Function(solver_obj.function_spaces.H_2d)
+    elev_init.project(-elev_amp*cos(2*pi*x_2d/lx))
+
+    tracer_init2d = None
+    if options.solve_tracer and constant_c == True:
+        tracer_init2d = Function(solver_obj.function_spaces.Q_2d, name='initial tracer')
+        tracer_init2d.assign(tracer_value)
+    if options.solve_tracer and constant_c == False:
+        tracer_init2d = Function(solver_obj.function_spaces.Q_2d, name='initial tracer')
+        x, y = SpatialCoordinate(solver_obj.mesh2d)
+        tracer_l = 0
+        tracer_r = 30.0
+        tracer_init2d.interpolate(tracer_l + (tracer_r - tracer_l)*0.5*(1.0 + sign(x - lx/2)))
+
+    solver_obj.assign_initial_conditions(elev=elev_init, tracer=tracer_init2d )
+    solver_obj.iterate()
+
+    # TODO do these checks every export ...
+    vol2d, vol2d_rerr = solver_obj.callbacks['export']['volume2d']()
+    assert vol2d_rerr < 1e-9 if options.element_family == 'rt-dg' else 1e-10, '2D volume is not conserved'
+    # if options.use_ale_moving_mesh:
+    #     vol3d, vol3d_rerr = solver_obj.callbacks['export']['volume3d']()
+    #     assert vol3d_rerr < 1e-10, '3D volume is not conserved'
+    if options.solve_tracer and constant == True:
+        tracer_int, tracer_int_rerr = solver_obj.callbacks['export']['tracer_2d mass']()
+        assert tracer_int_rerr < 1e-6, 'tracer is not conserved'
+        smin, smax, undershoot, overshoot = solver_obj.callbacks['export']['tracer_2d overshoot']()
+        max_abs_overshoot = max(abs(undershoot), abs(overshoot))
+        overshoot_tol = 1e-12
+        msg = 'Tracer overshoots are too large: {:}'.format(max_abs_overshoot)
+        assert max_abs_overshoot < overshoot_tol, msg
+
+
+""" Comment AA - reached up to here."""
+
+# @pytest.mark.parametrize('element_family', ['dg-dg',
+#                                             pytest.mark.not_travis(reason='travis timeout')('rt-dg')])
+# # @pytest.mark.parametrize('meshtype', ['regular',
+# #                                       pytest.mark.not_travis(reason='travis timeout')('sloped'),
+# #                                       pytest.mark.not_travis(reason='travis timeout')('warped')])
+# @pytest.mark.parametrize('timestepper_type', ['LeapFrog', 'SSPRK22'])
+def test_const_tracer(element_family, meshtype, timestepper_type):
+    """
+    Test ALE timeintegrators without slope limiters
+    One constant tracer, should remain constants
+    """
+    run_tracer_consistency(constant_c= True,
+                           use_nonlinear_equations=True,
+                           solve_tracer=True,
+                           use_limiter_for_tracers=True,
+                           no_exports=True)
+
+
+
+def test_nonconst_tracer():
+    """
+    Test CrankNicolson timeintegrator  with slope limiters
+    One constant and one non-trivial tracer, should see no overshoots
+    """
+    run_tracer_consistency(constant_c= False,
+                           use_nonlinear_equations=True,
+                           solve_tracer=True,
+                           use_limiter_for_tracers=True,
+                           no_exports=True)
+
+
+if __name__ == '__main__':
+    run_tracer_consistency(constant_c= False,
+                           use_nonlinear_equations=True,
+                           solve_tracer=True,
+                           use_limiter_for_tracers=True,
+                           no_exports=False)
