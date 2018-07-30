8aba44523f083cb3b4aae303d4f04724cef538fc
Tuomas Karna <tuomas.karna@gmail.com>
2018-07-30 14:12:57 +0300

    cre-plume: update time series interpolation test

--- examples/columbia_plume/timeseries_forcing.py
+++ examples/columbia_plume/timeseries_forcing.py
@@ -1,5 +1,5 @@
 """
-Implements time series interpolators that can be used as forcing terms.
+A test case for checking river flux time series interpolation.
 """
 import netCDF4
 import thetis.timezone as timezone
@@ -9,21 +9,30 @@ import datetime
 
 def test():
     sim_tz = timezone.FixedTimeZone(-8, 'PST')
-    init_date = datetime.datetime(1969, 12, 31, 16, tzinfo=sim_tz)
-    print('{:} {:}'.format(timezone.datetime_to_epoch(datetime.datetime(1970, 1, 1, tzinfo=timezone.utc_tz)), timezone.datetime_to_epoch(init_date)))
+    zero_date = datetime.datetime(1969, 12, 31, 16, tzinfo=sim_tz)
+    print('Epoch zero: {:} {:}'.format(
+        timezone.datetime_to_epoch(datetime.datetime(1970, 1, 1, tzinfo=timezone.pytz.utc)),
+        timezone.datetime_to_epoch(zero_date)))
 
-    print('{:} {:}'.format(timezone.datetime_to_epoch(datetime.datetime(2016, 5, 1, 8, tzinfo=timezone.utc_tz)), timezone.datetime_to_epoch(datetime.datetime(2016, 5, 1, tzinfo=sim_tz))))
+    init_date = datetime.datetime(2015, 5, 1, tzinfo=sim_tz)
+    print('sim. start: {:} {:}'.format(
+        timezone.datetime_to_epoch(datetime.datetime(2015, 5, 1, 8, tzinfo=timezone.pytz.utc)),
+        timezone.datetime_to_epoch(init_date)))
 
-    init_date = datetime.datetime(2016, 5, 1, tzinfo=sim_tz)
     river_flux_interp = interpolation.NetCDFTimeSeriesInterpolator(
         'forcings/stations/bvao3/bvao3.0.A.FLUX/*.nc',
         ['flux'], init_date, scalars=[-1.0], allow_gaps=True)
-    print('{:} {:}'.format(timezone.datetime_to_epoch(init_date), init_date, river_flux_interp(0.)))
+    print('interpolated: {:} {:} {:}'.format(timezone.datetime_to_epoch(init_date), init_date, river_flux_interp(0.)[0]))
 
-    ncd = netCDF4.Dataset('forcings/stations/bvao3/bvao3.0.A.FLUX/201605.nc')
+    ncd = netCDF4.Dataset('forcings/stations/bvao3/bvao3.0.A.FLUX/201505.nc')
     t = ncd['time'][:]
     v = ncd['flux'][:]
-    print('{:} {:} {:}'.format(t[0], datetime.datetime.fromtimestamp(t[0], tz=timezone.utc_tz), v[0]))
+    print('original:     {:} {:} {:}'.format(t[0], datetime.datetime.fromtimestamp(t[0], tz=timezone.pytz.utc), -v[0]))
+
+    dt = 900
+    for i in range(96):
+        d = init_date + datetime.timedelta(seconds=i*dt)
+        print('Time step {:3d}, {:}, flux: {:8.1f}'.format(i, d, river_flux_interp(i*dt)[0]))
 
 
 if __name__ == '__main__':
