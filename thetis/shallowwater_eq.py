7cb3c9d523040f835810acd2a9861d91c07d658c
jwallwork23 <j.wallwork16@imperial.ac.uk>
2018-08-02 19:55:55 +0100

    Attempted noslip edge residual

--- thetis/shallowwater_eq.py
+++ thetis/shallowwater_eq.py
@@ -1064,7 +1064,27 @@ class HUDivResidual(ShallowWaterContinuityResidualTerm):
         return -f
 
     def residual_edge(self, uv, eta, uv_old, eta_old, fields, fields_old, bnd_conditions):
-        return None
+        total_h = self.get_total_depth(eta_old)
+
+        f = 0
+        slip = False
+        mesh = uv.function_space().mesh()
+        P0 = FunctionSpace(mesh, "DG", 0)
+        p0_test = TestFunction(P0)
+
+        for bnd_marker in self.boundary_markers:
+            funcs = bnd_conditions.get(bnd_marker)
+            ds_bnd = ds(int(bnd_marker), degree=self.quad_degree)
+            if 'un' in funcs:
+               f += -total_h*dot(uv - funcs['un'], self.normal)*p0_test*ds_bnd
+               slip = True
+            else:
+               f += -total_h*dot(uv, self.normal)*p0_test*ds_bnd
+        if slip:
+            f += Constant(0.5) * jump(-total_h*uv, self.normal) * (p0_test('+') + p0_test('-')) * dS
+            return Function(P0).interpolate(assemble(-f))
+        else:
+            raise NotImplementedError	# TODO: consider other BCs
 
 
 class HorizontalAdvectionResidual(ShallowWaterMomentumResidualTerm):
