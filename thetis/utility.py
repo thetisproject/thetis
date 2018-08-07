84385e024d45fbba9bc50d92f320ddb2e637bbbe
Tuomas Karna <tuomas.karna@gmail.com>
2018-08-07 20:09:25 +0300

    ale: clear mesh spatial index after mesh update

--- thetis/utility.py
+++ thetis/utility.py
@@ -1203,6 +1203,7 @@ class ALEMeshUpdater(object):
         self.solver.mesh.coordinates.dat.data[:, 2] = new_z
         self.fields.z_coord_3d.dat.data[:] = new_z
         self.update_elem_height()
+        self.solver.mesh.clear_spatial_index()
 
 
 class ParabolicViscosity(object):
