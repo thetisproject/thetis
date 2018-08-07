4f1a7a49d4adc6431831aa2e113e9561494cb4a2
Tuomas Karna <tuomas.karna@gmail.com>
2018-08-07 14:58:20 +0300

    cre-plume: update tides and timeseries forcing for 2006 period

--- examples/columbia_plume/timeseries_forcing.py
+++ examples/columbia_plume/timeseries_forcing.py
@@ -14,17 +14,17 @@ def test():
         timezone.datetime_to_epoch(datetime.datetime(1970, 1, 1, tzinfo=timezone.pytz.utc)),
         timezone.datetime_to_epoch(zero_date)))
 
-    init_date = datetime.datetime(2015, 5, 1, tzinfo=sim_tz)
+    init_date = datetime.datetime(2006, 5, 15, tzinfo=sim_tz)
     print('sim. start: {:} {:}'.format(
-        timezone.datetime_to_epoch(datetime.datetime(2015, 5, 1, 8, tzinfo=timezone.pytz.utc)),
+        timezone.datetime_to_epoch(datetime.datetime(2006, 5, 15, 8, tzinfo=timezone.pytz.utc)),
         timezone.datetime_to_epoch(init_date)))
 
     river_flux_interp = interpolation.NetCDFTimeSeriesInterpolator(
-        'forcings/stations/bvao3/bvao3.0.A.FLUX/*.nc',
+        'forcings/stations/beaverarmy/flux_*.nc',
         ['flux'], init_date, scalars=[-1.0], allow_gaps=True)
     print('interpolated: {:} {:} {:}'.format(timezone.datetime_to_epoch(init_date), init_date, river_flux_interp(0.)[0]))
 
-    ncd = netCDF4.Dataset('forcings/stations/bvao3/bvao3.0.A.FLUX/201505.nc')
+    ncd = netCDF4.Dataset('forcings/stations/beaverarmy/flux_2006.nc')
     t = ncd['time'][:]
     v = ncd['flux'][:]
     print('original:     {:} {:} {:}'.format(t[0], datetime.datetime.fromtimestamp(t[0], tz=timezone.pytz.utc), -v[0]))
