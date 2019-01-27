35db065b187d3404f0fef78297b5107fdd5cebbd
Tuomas Karna <tuomas.karna@gmail.com>
2019-01-27 10:12:37 +0200

    bottom friction: update test case and plotting routines
    - update plot routine
    - add commandline args
    - use doubly period mesh
    - add more diagnostic plots
    - add custom model options
    - compute psi Schmidt number at run time
    
    Schmidt number has a high impact on Psi in the upper half of the water column.
    It's better to compute sigma_psi and use the standard kappa=0.4

--- examples/bottomFriction/plot_results.py
+++ examples/bottomFriction/plot_results.py
@@ -1,64 +1,134 @@
 """
 Plot result velocity etc profiles at steady state
 """
-from steadyChannel import *
-
-solver_obj.create_equations()
-
-from thetis.exporter import HDF5Exporter
+from thetis import *
+from steadyChannel import bottom_friction_test as run_test
+from steadyChannel import depth, surf_slope
 import numpy
+import matplotlib
+import matplotlib.pyplot as plt
+from collections import OrderedDict
 
 
-def load_function(var, export_ix):
-    func = solver_obj.fields[var]
-    fs = func.function_space()
-    fname = field_metadata[var]['filename']
-    h5reader = HDF5Exporter(fs, options.output_directory + '/hdf5', fname,
-                            next_export_ix=0, verbose=False)
-    h5reader.load(export_ix, func)
-    return func
+closure_abbr = {
+    'Generic Length Scale': 'gls',
+    'k-epsilon': 'k-epsilon',
+    'k-omega': 'k-omega',
+}
+
+stab_abbr = {
+    'Canuto A': 'CanutoA',
+    'Canuto B': 'CanutoB',
+    'Cheng': 'Cheng',
+}
+
+
+def unique(input_list):
+    """
+    Returns unique elements in a list
+    """
+    return list(OrderedDict.fromkeys(input_list))
+
+
+def parse_params(output_dir):
+    words = output_dir.split('_')
+    nz = int(words[1].replace('nz', ''))
+    gls_closure = words[2].replace('-', ' ').replace('k ', 'k-')
+    stability_func = words[3].replace('-', ' ')
+    return nz, gls_closure, stability_func
+
+
+output_dir_list = [
+    'outputs_nz250_Generic-Length-Scale_Canuto-A',
+]
+
+export_ix = 108
+
+
+def load_model(output_dir):
+    layers, gls_closure, stability_func = parse_params(output_dir)
+    solver_obj = run_test(
+        layers, gls_closure=gls_closure, stability_func=stability_func,
+        iterate=False, load_export_ix=export_ix, no_exports=True)
+
+    # add depth averaged velocity to uv
+    uv_3d = solver_obj.fields.uv_3d
+    uv_3d += solver_obj.fields.uv_dav_3d
+
+    entry = {
+        'solver': solver_obj,
+        'nz': layers,
+        'gls_closure': gls_closure,
+        'stability_func': stability_func,
+    }
+    return entry
+
+
+data = []
+for odir in output_dir_list:
+    entry = load_model(odir)
+    data.append(entry)
 
 
 # construct plot coordinates
-npoints = layers*6
-z_max = -(depth - 1e-10)
-z = numpy.linspace(0, z_max, npoints)
-x = numpy.zeros_like(z)
-y = numpy.zeros_like(z)
-xyz = numpy.vstack((x, y, z)).T
+def construct_plot_coordinates(layers):
+    offset = 1e-3
+    layer_h = depth/layers
+    z = numpy.arange(0, -depth, -layer_h)
+    z = numpy.vstack((z - offset, z - layer_h + offset)).T.ravel()
+    x = numpy.zeros_like(z)
+    y = numpy.zeros_like(z)
+    xyz = numpy.vstack((x, y, z)).T
+    return z, xyz
 
-import matplotlib
-import matplotlib.pyplot as plt
 
+layers = 100
+offset = depth/layers/2
+z = numpy.linspace(0 - offset, -depth + offset, layers)
 # compute analytical log profiles
 u_max = 0.9  # max velocity in [2] Fig 2.
-kappa = solver_obj.turbulence_model.options.kappa
+kappa = 0.4
 z_0 = physical_constants['z0_friction'].dat.data[0]
+# bottom friction velocity from u_max
 u_b = u_max * kappa / numpy.log((depth + z_0)/z_0)
+# analytical bottom friction velocity
+u_b = numpy.sqrt(physical_constants['g_grav'].dat.data[0] * depth * abs(surf_slope))
+
 u_log = u_b / kappa * numpy.log((z + depth + z_0)/z_0)
 # and viscosity profile
 nu = kappa*u_b*(z + z_0 + depth)*(-z/depth)
+# assuming that P - epsilon = 0
+eps = u_b**3/kappa/depth * (-z) / (z + depth + z_0)
+print('   u_b {:}'.format(u_b))
+print('max  u {:}'.format(u_log.max()))
+print('max nu {:}'.format(nu.max()))
 
 ana_data = {'uv_3d': (z, u_log),
             'eddy_visc_3d': (z, nu),
+            'eps_3d': (z[int(len(z)/3):-1], eps[int(len(z)/3):-1]),
             }
 
+xlim = {
+    'uv_3d': [0.0, 1.0],
+    'tke_3d': [0.0, 7e-3],
+    'eps_3d': [-1e-4, 9e-4],
+    'eddy_visc_3d': [0.0, 7e-2],
+    'len_3d': [0.0, 2.5],
+}
+
 # plot instantaneous profiles
-export_ix = 100
-varlist = ['uv_3d', 'tke_3d', 'eps_3d', 'eddy_visc_3d']
+varlist = ['uv_3d', 'tke_3d', 'eps_3d', 'len_3d', 'eddy_visc_3d']
+log_variables = []
 nplots = len(varlist)
-fig, axlist = plt.subplots(nrows=1, ncols=nplots, sharey=True, figsize=(nplots*2.3, 6))
+fig, axlist = plt.subplots(nrows=1, ncols=nplots, sharey=True,
+                           figsize=(nplots*2.3, 6))
+
+# plot analytical solutions
 for v, ax in zip(varlist, axlist):
-    func = load_function(v, export_ix)
-    arr = numpy.array(func.at(tuple(xyz)))
-    print('field: {:} min {:} max {:}'.format(v, arr.min(), arr.max()))
-    if len(arr.shape) == 2:
-        # take first component of vectors
-        arr = arr[:, 0]
-    ax.plot(arr, z, 'k', lw=1.5)
     if v in ana_data:
         zz, uu = ana_data[v]
-        ax.plot(uu, zz, 'r', lw=1.7, linestyle='dashed')
+        ax.plot(uu, zz, 'r', lw=1.7, linestyle='dashed',
+                label='analytical', zorder=10)
     ax.grid(True)
     ax.set_title(field_metadata[v]['shortname'].replace(' ', '\n'))
     ax.set_xlabel('[{:}]'.format(field_metadata[v]['unit']), horizontalalignment='right')
@@ -67,6 +137,90 @@ for v, ax in zip(varlist, axlist):
     fmt.set_powerlimits((-2, 3))
     ax.xaxis.set_major_locator(loc)
     ax.xaxis.set_major_formatter(fmt)
+    if v in xlim:
+        ax.set_xlim(xlim[v])
+
+
+def model_label(entry):
+    closure = entry['gls_closure']
+    return closure_abbr[closure] + ' ' + stab_abbr[entry['stability_func']]
+
+
+# plot_models
+arrays = {}
+for entry in data:
+    for v, ax in zip(varlist, axlist):
+        solver_obj = entry['solver']
+        gls_closure = entry['gls_closure']
+        stability_func = entry['stability_func']
+        layers = entry['nz']
+        z, xyz = construct_plot_coordinates(layers)
+        func = solver_obj.fields[v]
+        arr = numpy.array(func.at(tuple(xyz)))
+        arrays[v] = arr
+        print('field: {:} min {:} max {:}'.format(v, arr.min(), arr.max()))
+        if len(arr.shape) == 2:
+            # take first component of vectors
+            arr = arr[:, 0]
+        label = model_label(entry)
+        if v in log_variables:
+            ax.semilogx(arr, z, lw=1.5, ls='solid', label=label, alpha=0.7)
+        else:
+            ax.plot(arr, z, lw=1.5, ls='solid', label=label, alpha=0.7)
+
 axlist[0].set_ylabel('z [m]')
 axlist[0].set_ylim([-depth*1.005, 0])
-plt.savefig('friction_profiles.png', bbox_inches='tight', dpi=200.)
+
+# add legend
+plt.legend(loc='upper left', bbox_to_anchor=(1.02, 1.0))
+
+
+closure_str = '-'.join(sorted(unique([closure_abbr[e['gls_closure']].replace('-', '') for e in data])))
+stab_str = '-'.join(sorted(unique([stab_abbr[e['stability_func']] for e in data])))
+nz_str = '-'.join(sorted(unique([str(e['nz']) for e in data])))
+
+imgfile = 'profiles_{:}_{:}_nz{:}.png'.format(
+    closure_str, stab_str, nz_str)
+print('Saving figure {:}'.format(imgfile))
+plt.savefig(imgfile, bbox_inches='tight', dpi=200.)
+plt.close()
+
+# additional diagnostics plots
+
+fig, axlist = plt.subplots(nrows=1, ncols=nplots, sharey=True,
+                           figsize=(nplots*2.3, 6))
+z, xyz = construct_plot_coordinates(layers)
+m2 = numpy.array(solver_obj.fields.shear_freq_3d.at(tuple(xyz)))
+nu = numpy.array(solver_obj.fields.eddy_visc_3d.at(tuple(xyz)))
+dudz = numpy.sqrt(m2)
+eps = numpy.array(solver_obj.fields.eps_3d.at(tuple(xyz)))
+
+ax = axlist[0]
+ax.plot(dudz, z, lw=1.5, ls='solid', label='dudz', alpha=0.7)
+ax.legend()
+
+ax = axlist[1]
+ax.plot(nu, z, lw=1.5, ls='solid', label='nu', alpha=0.7)
+ax.legend()
+
+ax = axlist[2]
+nududz = nu*dudz
+ax.plot(nududz, z, lw=1.5, ls='solid', label='nu*dudz', alpha=0.7)
+ax.legend()
+
+ax = axlist[3]
+ax.plot(m2, z, lw=1.5, ls='solid', label='M2', alpha=0.7)
+ax.plot(dudz**2, z, lw=1.5, ls='solid', label='dudz2', alpha=0.7)
+ax.legend()
+
+ax = axlist[4]
+ax.plot(eps, z, lw=1.5, ls='solid', label='eps', alpha=0.7)
+p = nu*m2
+ax.plot(p, z, lw=1.5, ls='dashed', label='P', alpha=0.7)
+ax.legend()
+
+imgfile = 'diagnostics_{:}_{:}_nz{:}.png'.format(
+    closure_str, stab_str, nz_str)
+print('Saving figure {:}'.format(imgfile))
+plt.savefig(imgfile, bbox_inches='tight', dpi=200.)
+plt.close()
--- examples/bottomFriction/steadyChannel.py
+++ examples/bottomFriction/steadyChannel.py
@@ -25,108 +25,148 @@ import numpy
 
 physical_constants['z0_friction'].assign(1.5e-3)
 
-fast_convergence = False
-
-outputdir = 'outputs'
-# set mesh resolution
-dx = 2500.0
-layers = 250
 depth = 15.0
-
-nx = 3  # nb elements in flow direction
-lx = nx*dx
-ny = 2  # nb elements in cross direction
-ly = ny*dx
-mesh2d = PeriodicRectangleMesh(nx, ny, lx, ly, direction='x', reorder=True)
-
-dt = 25.0
-t_end = 12 * 3600.0  # sufficient to reach ~steady state
-if fast_convergence:
-    t_end = 5 * 3600.0
-t_export = 400.0
-u_mag = 1.0
-
-if os.getenv('THETIS_REGRESSION_TEST') is not None:
-    t_end = 5*t_export
-
-# bathymetry
-p1_2d = FunctionSpace(mesh2d, 'CG', 1)
-bathymetry2d = Function(p1_2d, name='Bathymetry')
-bathymetry2d.assign(depth)
-
-# create solver
-solver_obj = solver.FlowSolver(mesh2d, bathymetry2d, layers)
-options = solver_obj.options
-options.element_family = 'dg-dg'
-options.timestepper_type = 'SSPRK22'
-options.solve_salinity = False
-options.solve_temperature = False
-options.use_implicit_vertical_diffusion = True
-options.use_bottom_friction = True
-options.use_turbulence = True
-options.use_parabolic_viscosity = False
-options.vertical_viscosity = Constant(1.3e-6)  # background value
-options.vertical_diffusivity = Constant(1.4e-7)  # background value
-# options.use_ale_moving_mesh = False
-options.use_limiter_for_tracers = True
-options.simulation_export_time = t_export
-options.timestepper_options.use_automatic_timestep = False
-options.timestep = dt
-options.simulation_end_time = t_end
-options.horizontal_velocity_scale = Constant(u_mag)
-options.fields_to_export = ['uv_2d', 'elev_2d', 'elev_3d', 'uv_3d',
-                            'uv_dav_2d', 'uv_bottom_2d',
-                            'parab_visc_3d', 'eddy_visc_3d', 'shear_freq_3d',
-                            'tke_3d', 'psi_3d', 'eps_3d', 'len_3d', ]
-options.fields_to_export_hdf5 = ['uv_2d', 'elev_2d', 'uv_3d', 'uv_bottom_2d',
-                                 'eddy_visc_3d', 'eddy_diff_3d',
-                                 'shear_freq_3d',
-                                 'tke_3d', 'psi_3d', 'eps_3d', 'len_3d', ]
-turbulence_model_options = options.turbulence_model_options
-turbulence_model_options.apply_defaults('k-omega')
-turbulence_model_options.stability_function_name = 'Canuto B'
-layer_str = 'nz{:}'.format(layers)
-odir = '_'.join([outputdir, layer_str, turbulence_model_options.closure_name, turbulence_model_options.stability_function_name.replace(' ', '-')])
-options.output_directory = odir
-print_output('Exporting to ' + options.output_directory)
-
-solver_obj.create_function_spaces()
-
-# drive flow with momentum source term equivalent to constant surface slope
 surf_slope = -1.0e-5  # d elev/dx
-pressure_grad = -physical_constants['g_grav'] * surf_slope
-options.momentum_source_2d = Constant((pressure_grad, 0))
 
-solver_obj.create_equations()
 
-xyz = SpatialCoordinate(solver_obj.mesh)
-if fast_convergence:
-    # speed-up convergence by stating with u > 0
-    u_init_2d = 0.5
-    solver_obj.assign_initial_conditions(uv_2d=Constant((u_init_2d, 0)))
-    # consistent 3d velocity with slope
-    solver_obj.fields.uv_3d.project(as_vector((u_init_2d*0.3*(xyz[2]/depth + 0.5), 0, 0)))
+def bottom_friction_test(layers=25, gls_closure='k-omega',
+                         stability_func='Canuto B',
+                         fast_convergence=False, verify=True,
+                         iterate=True, load_export_ix=None, **model_options):
+    outputdir = 'outputs'
+    # set mesh resolution
+    dx = 2500.0
+
+    nx = 3  # nb elements in flow direction
+    lx = nx*dx
+    ny = 3  # nb elements in cross direction
+    ly = ny*dx
+    mesh2d = PeriodicRectangleMesh(nx, ny, lx, ly, direction='x', reorder=True)
+
+    dt = 25.0
+    t_end = 12 * 3600.0  # sufficient to reach ~steady state
+    if fast_convergence:
+        t_end = 5 * 3600.0
+    t_export = 400.0
+    u_mag = 1.0
+
+    if os.getenv('THETIS_REGRESSION_TEST') is not None:
+        t_end = 5*t_export
+
+    # bathymetry
+    p1_2d = FunctionSpace(mesh2d, 'CG', 1)
+    bathymetry2d = Function(p1_2d, name='Bathymetry')
+    bathymetry2d.assign(depth)
+
+    # create solver
+    solver_obj = solver.FlowSolver(mesh2d, bathymetry2d, layers)
+    options = solver_obj.options
+    options.element_family = 'dg-dg'
+    options.timestepper_type = 'SSPRK22'
+    options.solve_salinity = False
+    options.solve_temperature = False
+    options.use_implicit_vertical_diffusion = True
+    options.use_bottom_friction = True
+    options.use_turbulence = True
+    options.use_parabolic_viscosity = False
+    options.vertical_viscosity = Constant(1.3e-6)  # background value
+    options.vertical_diffusivity = Constant(1.4e-7)  # background value
+    # options.use_ale_moving_mesh = False
+    options.use_limiter_for_tracers = True
+    options.simulation_export_time = t_export
+    options.timestepper_options.use_automatic_timestep = False
+    options.timestep = dt
+    options.simulation_end_time = t_end
+    options.horizontal_velocity_scale = Constant(u_mag)
+    options.fields_to_export = ['uv_2d', 'elev_2d', 'elev_3d', 'uv_3d',
+                                'uv_dav_2d', 'uv_bottom_2d',
+                                'parab_visc_3d', 'eddy_visc_3d', 'shear_freq_3d',
+                                'tke_3d', 'psi_3d', 'eps_3d', 'len_3d', ]
+    options.fields_to_export_hdf5 = ['uv_2d', 'elev_2d', 'uv_3d', 'uv_bottom_2d',
+                                     'eddy_visc_3d', 'eddy_diff_3d',
+                                     'shear_freq_3d',
+                                     'tke_3d', 'psi_3d', 'eps_3d', 'len_3d', ]
+    options.update(model_options)
+    turb_options = options.turbulence_model_options
+    turb_options.apply_defaults(gls_closure)
+    turb_options.stability_function_name = stability_func
+    layer_str = 'nz{:}'.format(layers)
+    odir = '_'.join([outputdir, layer_str,
+                     turb_options.closure_name.replace(' ', '-'),
+                     turb_options.stability_function_name.replace(' ', '-')])
+    options.output_directory = odir
+
+    solver_obj.create_function_spaces()
+
+    # drive flow with momentum source term equivalent to constant surface slope
+    pressure_grad = -physical_constants['g_grav'] * surf_slope
+    options.momentum_source_2d = Constant((pressure_grad, 0))
+
+    solver_obj.create_equations()
+
+    xyz = SpatialCoordinate(solver_obj.mesh)
+    if fast_convergence:
+        # speed-up convergence by stating with u > 0
+        u_init_2d = 0.5
+        solver_obj.assign_initial_conditions(uv_2d=Constant((u_init_2d, 0)))
+        # consistent 3d velocity with slope
+        solver_obj.fields.uv_3d.project(as_vector((u_init_2d*0.3*(xyz[2]/depth + 0.5), 0, 0)))
+
+    if iterate:
+        print_output('Exporting to ' + options.output_directory)
+        solver_obj.iterate()
+
+        if verify and os.getenv('THETIS_REGRESSION_TEST') is None:
+            # compare against logarithmic velocity profile
+            # u = u_b / kappa * log((z + bath + z_0)/z_0)
+            # estimate bottom friction velocity from maximal u
+            u_max = 0.9  # max velocity in [2] Fig 2.
+            l2_tol = 0.05
+            kappa = solver_obj.options.turbulence_model_options.kappa
+            z_0 = physical_constants['z0_friction'].dat.data[0]
+            u_b = u_max * kappa / np.log((depth + z_0)/z_0)
+            log_uv = Function(solver_obj.function_spaces.P1DGv, name='log velocity')
+            log_uv.project(as_vector((u_b / kappa * ln((xyz[2] + depth + z_0)/z_0), 0, 0)))
+            out = File(options.output_directory + '/log_uv.pvd')
+            out.write(log_uv)
+
+            uv_p1_dg = Function(solver_obj.function_spaces.P1DGv, name='velocity p1dg')
+            uv_p1_dg.project(solver_obj.fields.uv_3d + solver_obj.fields.uv_dav_3d)
+            volume = lx*ly*depth
+            uv_l2_err = errornorm(log_uv, uv_p1_dg)/numpy.sqrt(volume)
+            assert uv_l2_err < l2_tol, 'L2 error is too large: {:} > {:}'.format(uv_l2_err, l2_tol)
+            print_output('L2 error {:.4f} PASSED'.format(uv_l2_err))
+    elif load_export_ix is not None:
+        print_output('Loading state: {:}'.format(load_export_ix))
+        solver_obj.load_state(load_export_ix)
+
+    return solver_obj
+
 
 if __name__ == '__main__':
-    solver_obj.iterate()
+    import argparse
 
-    if os.getenv('THETIS_REGRESSION_TEST') is None:
-        # compare against logarithmic velocity profile
-        # u = u_b / kappa * log((z + bath + z_0)/z_0)
-        # estimate bottom friction velocity from maximal u
-        u_max = 0.9  # max velocity in [2] Fig 2.
-        l2_tol = 0.05
-        kappa = solver_obj.options.turbulence_model_options.kappa
-        z_0 = physical_constants['z0_friction'].dat.data[0]
-        u_b = u_max * kappa / np.log((depth + z_0)/z_0)
-        log_uv = Function(solver_obj.function_spaces.P1DGv, name='log velocity')
-        log_uv.project(as_vector((u_b / kappa * ln((xyz[2] + depth + z_0)/z_0), 0, 0)))
-        out = File(options.output_directory + '/log_uv.pvd')
-        out.write(log_uv)
+    parser = argparse.ArgumentParser(
+        description='Run bottom friction test case',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument('-n', '--nlevels', type=int, default=50,
+                        help='number of vertical levels')
+    parser.add_argument('-m', '--model', default='k-epsilon',
+                        choices=['k-epsilon', 'k-omega', 'gls'],
+                        help='GLS turbulence closure model')
+    parser.add_argument('-s', '--stability-func', default='Canuto-A',
+                        choices=['Canuto-A', 'Canuto-B', 'Cheng'],
+                        help='Stability function name')
+    parser.add_argument('-v', '--verify', action='store_true',
+                        help='Verify correctness against log profile.')
 
-        uv_p1_dg = Function(solver_obj.function_spaces.P1DGv, name='velocity p1dg')
-        uv_p1_dg.project(solver_obj.fields.uv_3d + solver_obj.fields.uv_dav_3d)
-        volume = lx*ly*depth
-        uv_l2_err = errornorm(log_uv, uv_p1_dg)/numpy.sqrt(volume)
-        assert uv_l2_err < l2_tol, 'L2 error is too large: {:} > {:}'.format(uv_l2_err, l2_tol)
-        print_output('L2 error {:.4f} PASSED'.format(uv_l2_err))
+    args = parser.parse_args()
+    model = args.model
+    if model == 'gls':
+        model = 'Generic Length Scale'
+    bottom_friction_test(
+        layers=args.nlevels,
+        gls_closure=model,
+        stability_func=args.stability_func.replace('-', ' '),
+        verify=args.verify)
