0832fddaf317cd839a1e947183b35b6de1264afd
Thanasis Angeloudis <a.angeloudis@ed.ac.uk>
2019-06-12 13:06:32 +0100

    adding discrete turbine example

--- examples/discrete_turbines/qmesh/tidal_stream_array_mesh.py
+++ examples/discrete_turbines/qmesh/tidal_stream_array_mesh.py
@@ -3,20 +3,20 @@ def mesh():
     # Reading in the shapefile describing the domain boundaries, and creating a gmsh file.
     boundaries = qmesh.vector.Shapes()
     boundaries.fromFile('outline.shp')
-    loopShapes = qmesh.vector.identifyLoops(boundaries,isGlobal=False, defaultPhysID=1000, fixOpenLoops=True)
+    loopShapes = qmesh.vector.identifyLoops(boundaries, isGlobal=False, defaultPhysID=1000, fixOpenLoops=True)
     polygonShapes = qmesh.vector.identifyPolygons(loopShapes, smallestNotMeshedArea=100, meshedAreaPhysID=1)
 
     inner_plot_lines = qmesh.vector.Shapes()
     inner_plot_lines.fromFile('turbine_circles.shp')
-    inner_plot_loops = qmesh.vector.identifyLoops(inner_plot_lines,fixOpenLoops=True, extraPointsPerVertex=10)
-    inner_plot_polygons = qmesh.vector.identifyPolygons(inner_plot_loops, meshedAreaPhysID = 1)
+    inner_plot_loops = qmesh.vector.identifyLoops(inner_plot_lines, fixOpenLoops=True, extraPointsPerVertex=10)
+    inner_plot_polygons = qmesh.vector.identifyPolygons(inner_plot_loops, meshedAreaPhysID=1)
 
     gradation = []
     gradation.append(qmesh.raster.meshMetricTools.gradationToShapes())
     gradation[-1].setShapes(inner_plot_polygons)
     gradation[-1].setRasterBounds(-100, 800., -100, 400.0)
-    gradation[-1].setRasterResolution(450,150)
-    gradation[-1].setGradationParameters(4.0, 50.0, 500.0,10)
+    gradation[-1].setRasterResolution(450, 150)
+    gradation[-1].setGradationParameters(4.0, 50.0, 500.0, 10)
     gradation[-1].calculateLinearGradation()
     gradation[-1].writeNetCDF('gradation_to_turbine.nc')
     # gradation[-1].fromFile('gradation_to_turbineplot.nc')
@@ -25,14 +25,14 @@ def mesh():
     if len(gradation) == 1:
         meshMetricRaster = gradation[0]
     else:
-        meshMetricRaster = qmesh.raster.meshMetricTools.minimumRaster(gradation) #grad_0
+        meshMetricRaster = qmesh.raster.meshMetricTools.minimumRaster(gradation)
 
     meshMetricRaster.writeNetCDF('meshMetric.nc')
 
     # Create domain object and write gmsh files.
     domain = qmesh.mesh.Domain()
     domainLines, domainPolygons = qmesh.vector.insertRegions(loopShapes, polygonShapes, inner_plot_loops, inner_plot_polygons)
-    domain.setGeometry(domainLines,domainPolygons)
+    domain.setGeometry(domainLines, domainPolygons)
     domain.setMeshMetricField(meshMetricRaster)
     domain.setTargetCoordRefSystem('EPSG:32630', fldFillValue=1000.0)
     # Meshing
@@ -40,11 +40,13 @@ def mesh():
                 fldFilename='tidal_mesh.fld',
                 mshFilename='tidal_mesh.msh')
 
+
 def convertMesh(meshname):
     '''Todo: add docstring '''
     mesh = qmesh.mesh.Mesh()
     mesh.readGmsh(meshname+'.msh', 'EPSG:32630')
     mesh.writeShapefile(meshname)
+
 
 if __name__ == '__main__':
     import qmesh
