b13574ce062c5f91725438239df498ba06d23eb6
Mariana Clare <mdac12@hotmail.co.uk>
2020-03-04 11:37:42 +0000

    update test case

--- test/tracerEq/test_h-advection_mes_2d.py
+++ test/tracerEq/test_h-advection_mes_2d.py
@@ -48,7 +48,7 @@ def run(refinement, **model_options):
     options.simulation_end_time = t_end
     options.simulation_export_time = t_export
     solverobj.create_function_spaces()
-    corr_factor = Function(solverobj.function_spaces.H_2d, name='uv tracer factor')
+    corr_factor = Function(solverobj.function_spaces.H_2d, name='uv tracer factor').interpolate(Constant(1.0))
     options.tracer_advective_velocity_factor = corr_factor
     options.solve_tracer = True
     options.use_limiter_for_tracers = True
@@ -96,8 +96,6 @@ def run(refinement, **model_options):
 
     # custom time loop that solves tracer equation only
     ti = solverobj.timestepper.timesteppers.tracer
-
-    uv_tracer.assign(uv_init)
 
     i = 0
     iexport = 1
