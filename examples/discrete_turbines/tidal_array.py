c5179e08aa7b82e7bb58e0e8098af9164eb33f3f
Stephan Kramer <s.kramer@imperial.ac.uk>
2020-04-16 20:04:16 +0100

    Update petsc option

--- examples/discrete_turbines/tidal_array.py
+++ examples/discrete_turbines/tidal_array.py
@@ -50,7 +50,7 @@ options.timestepper_options.solver_parameters = {
     'snes_max_it': 20,
     'ksp_type': 'preonly',
     'pc_type': 'lu',
-    'pc_factor_mat_solver_package': 'mumps',
+    'pc_factor_mat_solver_type': 'mumps',
 }
 
 # Boundary conditions - Steady state case
