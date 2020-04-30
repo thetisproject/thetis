e4c410a93b03720456fe2dda980506afa1646621
Joe <j.wallwork16@imperial.ac.uk>
2020-04-30 13:43:28 +0100

    Update swe/test_anisotropic (#204)
    * Neater abspath
    
    * Use 2turbine setup
    
    * snes iteration test and other FE spaces
    
    * Remark on outflow condition

--- test/swe2d/test_anisotropic.py
+++ test/swe2d/test_anisotropic.py
@@ -1,27 +1,47 @@
+"""
+Two turbine array
+=================
+
+A simple two turbine array is positioned in a rectangular channel which experiences a uniform inflow
+velocity at the left hand boundary. These turbines act as momentum sinks, since they extract energy
+from the system.
+
+The setup corresponds to the 'offset' configuration considered in the numerical experiments section
+of [1]. The main contributions of [1] are the derivation of a goal-oriented error estimate for
+shallow water modelling and subsequent implementation of mesh adaptation algorithms. An adapted mesh
+resulting from this process is used in this test. The mesh is anisotropic in the flow direction.
+
+If the default SIPG parameter is used, this steady state problem fails to converge under some
+finite element pairs. The automatic SIPG parameter functionality provided by Thetis should ensure
+converge where it was not attained and reduce the number of nonlinear solver iterations otherwise.
+
+[1] J.G. Wallwork, N. Barral, S.C. Kramer, D.A. Ham, M.D. Piggott, "Goal-Oriented Error Estimation
+    and Mesh Adaptation in Shallow Water Modelling" (2020), Springer Nature Applied Sciences (to
+    appear).
+"""
 from thetis import *
 from firedrake.petsc import PETSc
 import pytest
 import os
 
 
-def run_steady_turbine(**model_options):
-    """
-    Consider a simple test case with two turbines positioned in a channel. The mesh has been adapted
-    with respect to fluid speed and so has strong anisotropy in the direction of flow.
+def run(**model_options):
 
-    If the default SIPG parameter is used, this steady state problem fails to converge. However,
-    using the automatic SIPG parameter functionality, it should converge.
-    """
-
-    # Load an anisotropic mesh from file
+    # load an anisotropic mesh from file
     plex = PETSc.DMPlex().create()
-    abspath = os.path.realpath(__file__)
-    plex.createFromFile(abspath.replace('test_anisotropic.py', 'anisotropic_plex.h5'))
+    plex.createFromFile(os.path.join(os.path.dirname(__file__), 'anisotropic_plex.h5'))
     mesh2d = Mesh(plex)
-    x, y = SpatialCoordinate(mesh2d)
+    P1_2d = FunctionSpace(mesh2d, "CG", 1)
 
-    # Create steady state solver object
-    solver_obj = solver2d.FlowSolver2d(mesh2d, Constant(40.0))
+    # physics
+    viscosity = Constant(0.5)
+    inflow_velocity = Constant(as_vector([0.5, 0.0]))
+    depth = 40.0
+    drag_coefficient = Constant(0.0025)
+    bathymetry2d = Function(P1_2d).assign(depth)
+
+    # create steady state solver object
+    solver_obj = solver2d.FlowSolver2d(mesh2d, bathymetry2d)
     options = solver_obj.options
     options.timestep = 20.0
     options.simulation_export_time = 20.0
@@ -30,78 +50,102 @@ def run_steady_turbine(**model_options):
     options.timestepper_options.solver_parameters = {
         'mat_type': 'aij',
         'snes_type': 'newtonls',
+        'snes_linesearch_type': 'bt',
         'snes_rtol': 1e-8,
+        'snes_max_it': 100,
         'snes_monitor': None,
+        'snes_converged_reason': None,
+        'ksp_type': 'preonly',
+        'ksp_converged_reason': None,
         'pc_type': 'lu',
         'pc_factor_mat_solver_type': 'mumps',
     }
     options.output_directory = 'outputs'
     options.fields_to_export = ['uv_2d', 'elev_2d']
     options.use_grad_div_viscosity_term = False
-    options.element_family = 'dg-dg'
-    options.horizontal_viscosity = Constant(1.0)
-    options.quadratic_drag_coefficient = Constant(0.0025)
+    options.element_family = 'dg-cg'
+    options.horizontal_viscosity = viscosity
+    options.quadratic_drag_coefficient = drag_coefficient
     options.use_lax_friedrichs_velocity = True
     options.lax_friedrichs_velocity_scaling_factor = Constant(1.0)
     options.use_grad_depth_viscosity_term = False
     options.update(model_options)
     solver_obj.create_equations()
 
-    # Apply boundary conditions
+    # apply boundary conditions
     solver_obj.bnd_functions['shallow_water'] = {
-        1: {'uv': Constant([3.0, 0.0])},
-        2: {'elev': Constant(0.0)},
-        3: {'un': Constant(0.0)},
+        1: {'uv': inflow_velocity},  # inflow condition used upstream
+        2: {'elev': Constant(0.0)},  # we need to impose a condition on elevation to close system
+        3: {'un': Constant(0.0)},    # freeslip on channel walls
     }
 
-    def bump(fs, locs, scale=1.0):
-        """Scaled bump function for turbines."""
-        i = 0
-        for j in range(len(locs)):
-            x0 = locs[j][0]
-            y0 = locs[j][1]
-            r = locs[j][2]
-            expr1 = (x-x0)*(x-x0) + (y-y0)*(y-y0)
-            expr2 = scale*exp(1 - 1/(1 - (x-x0)*(x-x0)/r**2))*exp(1 - 1/(1 - (y-y0)*(y-y0)/r**2))
-            i += conditional(lt(expr1, r*r), expr2, 0)
-        return i
+    # --- setup turbine array
 
-    # Set up turbine array
-    L = 1000.0       # domain length
-    W = 300.0        # domain width
+    L = 1200.0       # domain length
+    W = 500.0        # domain width
     D = 18.0         # turbine diameter
     A = pi*(D/2)**2  # turbine area
-    locs = [(L/2-8*D, W/2, D/2), (L/2+8*D, W/2, D/2)]  # turbine locations
+    S = 8            # turbine separation in x-direction
 
+    # turbine locations
+    locs = [(L/2-S*D, W/2, D/2), (L/2+S*D, W/2, D/2)]
+
+    def bump(mesh, locs, scale=1.0):
+        """
+        Smooth approximation to indicator function used to represent tidal turbines.
+        Scaled bump function for turbines.
+
+        :arg locs: a list of (x, y, r) triples, each of which describing a single turbine in the
+                   array. (x, y) gives the centre of the turbine and r gives its radius.
+        :kwarg scale: optional scaling parameter which is useful for normalisation.
+        """
+        x, y = SpatialCoordinate(mesh)
+        i = 0
+        for j in range(len(locs)):
+            x0, y0, r = locs[j]
+            expr = scale*exp(1.0 - 1.0/(1.0 - ((x-x0)/r)**2))*exp(1.0 - 1.0/(1.0 - ((y-y0)/r)**2))
+            i += conditional(lt((x-x0)**2 + (y-y0)**2, r*r), expr, 0)
+        return i
+
+    thrust_coefficient = 0.8
     # NOTE: We include a correction to account for the fact that the thrust coefficient is based
     #       on an upstream velocity, whereas we are using a depth averaged at-the-turbine velocity
     #       (see Kramer and Piggott 2016, eq. (15)).
-    correction = 4/(1 + sqrt(1-A/(40.0*D)))**2
+    correction = 4.0/(1.0 + sqrt(1.0 - A/(depth*D)))**2
     scaling = len(locs)/assemble(bump(solver_obj.function_spaces.P1DG_2d, locs)*dx)
     farm_options = TidalTurbineFarmOptions()
     farm_options.turbine_density = bump(solver_obj.function_spaces.P1DG_2d, locs, scale=scaling)
     farm_options.turbine_options.diameter = D
-    farm_options.turbine_options.thrust_coefficient = 0.8*correction
+    farm_options.turbine_options.thrust_coefficient = thrust_coefficient*correction
     solver_obj.options.tidal_turbine_farms['everywhere'] = farm_options
 
-    # Apply initial guess of inflow velocity and solve
-    solver_obj.assign_initial_conditions(uv=Constant([3.0, 0.0]))
+    # apply initial guess of inflow velocity, solve and return number of nonlinear solver iterations
+    solver_obj.assign_initial_conditions(uv=inflow_velocity)
     solver_obj.iterate()
+    return solver_obj.timestepper.solver.snes.getIterationNumber()
 
 
 # ---------------------------
 # standard tests for pytest
 # ---------------------------
 
-@pytest.fixture(params=[True, False])
-def auto_sipg(request):
+@pytest.fixture(params=['dg-cg', 'dg-dg', 'rt-dg'])
+def family(request):
     return request.param
 
 
-def test_sipg(auto_sipg):
-    if not auto_sipg:
-        pytest.xfail("The default SIPG parameter is not sufficient for this problem.")
-    run_steady_turbine(use_automatic_sipg_parameter=auto_sipg, no_exports=True)
+def test_sipg(family):
+    options = {'element_family': family, 'no_exports': True}
+    snes_it_auto = run(use_automatic_sipg_parameter=True, **options)
+    try:
+        snes_it_default = run(use_automatic_sipg_parameter=False, **options)
+        msg = "auto-sipg: snes iterations was not reduced, expected {:d} < {:d}"
+        assert snes_it_auto < snes_it_default, msg.format(snes_it_auto, snes_it_default)
+        msg = "auto-sipg: snes iterations reduced from {:d} to {:d} for {:s} PASSED"
+        print_output(msg.format(snes_it_default, snes_it_auto, family))
+    except ConvergenceError:
+        msg = "auto-sipg: snes converged where it diverged under default for {:s} PASSED"
+        print_output(msg.format(family))
 
 # ---------------------------
 # run individual setup for debugging
@@ -109,4 +153,4 @@ def test_sipg(auto_sipg):
 
 
 if __name__ == '__main__':
-    run_steady_turbine(use_automatic_sipg_parameter=False, no_exports=False)
+    print_output(run(use_automatic_sipg_parameter=False, no_exports=False, element_family='rt-dg'))
