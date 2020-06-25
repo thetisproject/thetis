dde824a5144035a39744f8a92053e5352452bfb5
Stephan Kramer <s.kramer@imperial.ac.uk>
2020-06-25 18:43:55 +0100

    Several fixes in cleanup

--- thetis/__init__.py
+++ thetis/__init__.py
@@ -16,7 +16,6 @@ import thetis.optimisation  # NOQA
 from thetis._version import get_versions
 from thetis.assembledschur import AssembledSchurPC  # NOQA
 from thetis.options import TidalTurbineFarmOptions, DiscreteTidalTurbineFarmOptions  # NOQA
-from thetis.turbines import DiscreteTurbineOperation  # NOQA
 import os  # NOQA
 import datetime  # NOQA
 
