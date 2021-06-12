50b8012b9d0b75c24006d52450f774581698c32b
Tuomas Karna <tuomas.karna@gmail.com>
2021-06-12 12:11:26 +0300

    solver3d: fix VerticalIntegrator for hdiv spaces
    - tune solver parameters for hdiv case to ensure convergence

--- thetis/utility.py
+++ thetis/utility.py
@@ -486,8 +486,14 @@ class VerticalIntegrator(object):
         if solver_parameters is None:
             solver_parameters = {}
         solver_parameters.setdefault('snes_type', 'ksponly')
-        if e_continuity.vertical != 'hdiv':
+        if e_continuity.horizontal == 'dg':
             solver_parameters.setdefault('ksp_type', 'preonly')
+            solver_parameters.setdefault('pc_type', 'bjacobi')
+            solver_parameters.setdefault('sub_ksp_type', 'preonly')
+            solver_parameters.setdefault('sub_pc_type', 'ilu')
+        else:
+            solver_parameters.setdefault('ksp_type', 'cg')
+            solver_parameters.setdefault('ksp_rtol', 1e-14)
             solver_parameters.setdefault('pc_type', 'bjacobi')
             solver_parameters.setdefault('sub_ksp_type', 'preonly')
             solver_parameters.setdefault('sub_pc_type', 'ilu')
