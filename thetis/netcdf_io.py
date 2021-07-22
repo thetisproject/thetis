6f3194af966563790d9cd3f73968f380c9b461d4
Tuomas Karna <tuomas.karna@gmail.com>
2021-07-22 13:21:51 +0300

    fixes for quad mesh

--- thetis/netcdf_io.py
+++ thetis/netcdf_io.py
@@ -13,13 +13,13 @@ def write_nc_field(function, filename, fieldname):
     """
     fs = function.function_space()
     mesh = fs.mesh()
-    is_vector = fs.shape != ()
-    if is_vector:
-        vector_dim = fs.shape[0]
 
-    fs_p0 = get_functionspace(mesh, 'DG', 0)
-    fs_p1 = get_functionspace(mesh, 'CG', 1)
-    fs_p1dg = get_functionspace(mesh, 'DG', 1)
+    tri_mesh = mesh.ufl_cell() == fd.triangle
+    dg_family = 'DG' if tri_mesh else 'DQ'
+    cg_family = 'CG' if tri_mesh else 'Q'
+    fs_p0 = get_functionspace(mesh, dg_family, 0)
+    fs_p1 = get_functionspace(mesh, cg_family, 1)
+    fs_p1dg = get_functionspace(mesh, dg_family, 1)
 
     elem = fs.ufl_element()
     family = elem.family()
@@ -37,6 +37,9 @@ def write_nc_field(function, filename, fieldname):
     data_at_cells = degree == 0
     is_dg = family in ['Discontinuous Lagrange', 'DQ']
     fs_vertex = fs_p1dg if (is_dg and degree == 1) else fs_p1
+    is_vector = fs.shape != ()
+    if is_vector:
+        vector_dim = fs.shape[0]
     face_nodes = elem.cell().num_vertices()
 
     gdim = mesh.geometric_dimension()
@@ -45,18 +48,10 @@ def write_nc_field(function, filename, fieldname):
         raise NotImplementedError(f'Fields of dimension {gdim} are not supported')
     if gdim != 2:
         raise NotImplementedError(f'Mesh coordinates of dimension {gdim} are not supported')
-    # define mesh connectivity and coordinates
+
+    # define mesh connectivity
     global_nb_cells = fs_p0.dim()
     global_nb_vertices = fs_vertex.dim()
-
-    # make coordinate field
-    elem = fs_vertex.ufl_element()
-    family = elem.family()
-    degree = elem.degree()
-    fs_coords = get_functionspace(mesh, family, degree, vector=True)
-    f_coords = fd.Function(fs_coords)
-    x, y = fd.SpatialCoordinate(mesh)
-    f_coords.interpolate(fd.as_vector((x, y)))
 
     def get_lg_index(fs):
         local_nb_dofs = fs.dof_dset.size * fs.dof_dset.cdim
@@ -66,12 +61,19 @@ def write_nc_field(function, filename, fieldname):
 
     local2global_cell_ix = get_lg_index(fs_p0)
     local2global_vertex_ix = get_lg_index(fs_vertex)
-
     local2global_map = fs_vertex.dof_dset.lgmap
-
     local_nb_cells = fs_p0.dof_dset.size * fs_p0.dof_dset.cdim
     local_cell_nodes = fs_vertex.cell_node_list[:local_nb_cells, :]
     global_cell_nodes = local2global_map.apply(local_cell_nodes).reshape(local_cell_nodes.shape)
+
+    # make coordinate field
+    elem = fs_vertex.ufl_element()
+    family = elem.family()
+    degree = elem.degree()
+    fs_coords = get_functionspace(mesh, family, degree, vector=True)
+    f_coords = fd.Function(fs_coords)
+    x, y = fd.SpatialCoordinate(mesh)
+    f_coords.interpolate(fd.as_vector((x, y)))
 
     with netCDF4.Dataset(filename, 'w', parallel=True) as ncfile:
         ncfile.createDimension('face', global_nb_cells)
