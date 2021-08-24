70673326c0a36c2ecbfa469d6611eabef3bfb0c8
Tuomas Karna <tuomas.karna@gmail.com>
2021-08-24 15:37:44 +0300

    add channel inversion example and inversion tools

---
+++ examples/channel_inversion/inverse_problem.py
@@ -0,0 +1,162 @@
+from thetis import *
+from firedrake_adjoint import *
+import numpy
+import thetis.inversion_tools as inversion_tools
+from model_config import construct_solver
+
+solver_obj = construct_solver(
+    output_directory='outputs', store_station_time_series=False)
+options = solver_obj.options
+mesh2d = solver_obj.mesh2d
+bathymetry_2d = solver_obj.fields.bathymetry_2d
+manning_2d = solver_obj.fields.manning_2d
+elev_init_2d = solver_obj.fields.elev_init_2d
+
+# choose control variable(s)
+controls = [
+    # 'Bathymetry',
+    'Manning',
+    # 'InitialElev',
+]
+output_dir_suffix = '_' + '-'.join(controls) + '-opt'
+options.output_directory += output_dir_suffix
+
+gamma_hessian_list = []
+control_bounds_list = []
+op = inversion_tools.OptimisationProgress(options.output_directory)
+
+for control_name in controls:
+    if control_name == 'Bathymetry':
+        # FIXME move allocation to model_config
+        bathymetry_2d.assign(5.0)
+        bounds = [1.0, 50.]
+        op.add_control(bathymetry_2d)
+        gamma_hessian = Constant(1e-3)  # regularization parameter
+    elif control_name == 'Manning':
+        manning_2d.assign(1.0e-03)
+        bounds = [1e-4, 1e-1]
+        op.add_control(manning_2d)
+        gamma_hessian = Constant(1.0)
+    elif control_name == 'InitialElev':
+        elev_init_2d.assign(0.5)
+        bounds = [-10., 10.]
+        op.add_control(elev_init_2d)
+        gamma_hessian = Constant(0.1)
+    else:
+        raise ValueError(f'Unsupported control variable {control_name}')
+    print_output(f'{control_name} regularization params: hess={float(gamma_hessian):.3g}')
+    gamma_hessian_list.append(gamma_hessian)
+    control_bounds_list.append(bounds)
+control_bounds = numpy.array(control_bounds_list).T  # [[lo1, lo2], [hi1, hi2]]
+
+print_output('Exporting to ' + options.output_directory)
+solver_obj.assign_initial_conditions(elev=elev_init_2d, uv=Constant((1e-5, 0)))
+
+# define the cost function
+# scale cost function so that J ~ O(1)
+dt_const = solver_obj.dt
+total_time_const = options.simulation_end_time
+J_scalar = dt_const/total_time_const
+
+observation_data_dir = 'outputs_forward'
+variable = 'elev'
+station_names = [
+    'stationA',
+    'stationB',
+    'stationC',
+    'stationD',
+    'stationE',
+]
+stationmanager = inversion_tools.StationObservationManager(
+    mesh2d, J_scalar=J_scalar, output_directory=options.output_directory)
+stationmanager.load_observation_data(observation_data_dir, station_names, variable)
+stationmanager.set_model_field(solver_obj.fields.elev_2d)
+
+# regularization for each control field
+reg_manager = inversion_tools.ControlRegularizationManager(
+    op.control_coeff_list, gamma_hessian_list, J_scalar=J_scalar)
+
+
+def cost_function():
+    """
+    Compute square misfit between data and observations.
+    """
+    t = solver_obj.simulation_time
+
+    J_misfit = stationmanager.eval_cost_function(t)
+    # J_regularization = regularizationmanager.eval_cost_function()
+    op.J += J_misfit  # + J_regularization
+
+
+def gradient_eval_callback(j, djdm, m):
+    """
+    Stash optimisation state.
+    """
+    op.set_control_state(j, djdm, m)
+    op.nb_grad_evals += 1
+
+
+# compute regularization term
+op.J = reg_manager.eval_cost_function()
+
+# Solve and setup reduced functional
+solver_obj.iterate(export_func=cost_function)
+Jhat = ReducedFunctional(op.J, op.control_list, derivative_cb_post=gradient_eval_callback)
+stop_annotating()
+
+do_consistency_test = True
+do_taylor_test = True
+
+# Consistency test
+if do_consistency_test:
+    print_output('Running consistency test')
+    J = Jhat(op.control_coeff_list)
+    assert numpy.isclose(J, op.J)
+    print_output('Consistency test passed!')
+
+# Taylor test
+if do_taylor_test:
+    func_list = []
+    for f in op.control_coeff_list:
+        dc = Function(f.function_space()).assign(f)
+        func_list.append(dc)
+    minconv = taylor_test(Jhat, op.control_coeff_list, dc)
+    assert minconv > 1.9
+    print_output('Taylor test passed!')
+
+
+def optimization_callback(m):
+    """
+    Stash optimisation progress after successful line search.
+    """
+    op.update_progress()
+    stationmanager.dump_time_series()
+
+
+# Run inversion
+opt_method = 'L-BFGS-B'
+opt_verbose = -1  # print scipy diagnostics -1, 0, 1, 99, 100, 101
+opt_options = {
+    'maxiter': 15,
+    'ftol': 1e-5,
+    'disp': opt_verbose if mesh2d.comm.rank == 0 else -1,
+}
+if os.getenv('THETIS_REGRESSION_TEST') is not None:
+    opt_options['maxiter'] = 1
+
+print_output(f'Running {opt_method} optimization')
+op.reset_counters()
+op.start_clock()
+J = float(Jhat(op.control_coeff_list))
+op.set_initial_state(J, Jhat.derivative(), op.control_coeff_list)
+stationmanager.dump_time_series()
+control_opt_list = minimize(
+    Jhat, method=opt_method, bounds=control_bounds,
+    callback=optimization_callback, options=opt_options)
+if isinstance(control_opt_list, Function):
+    control_opt_list = [control_opt_list]
+for oc, cc in zip(control_opt_list, op.control_coeff_list):
+    name = cc.name()
+    oc.rename(name)
+    print_function_value_range(oc, prefix='Optimal')
+    File(f'{options.output_directory}/{name}_optimised.pvd').write(oc)
---
+++ examples/channel_inversion/model_config.py
@@ -0,0 +1,84 @@
+from thetis import *
+
+
+def construct_solver(store_station_time_series=True, **model_options):
+    lx = 100e3
+    nx = 30
+    delta_x = lx/nx
+    ny = 2
+    ly = delta_x * ny
+    mesh2d = RectangleMesh(nx, ny, lx, ly)
+
+    t_end = 8 * 3600.
+    u_mag = Constant(6.0)
+    t_export = 600.
+    dt = 600.
+
+    if os.getenv('THETIS_REGRESSION_TEST') is not None:
+        t_end = 5*t_export
+
+    # bathymetry
+    P1_2d = get_functionspace(mesh2d, 'CG', 1)
+    bathymetry_2d = Function(P1_2d, name='Bathymetry')
+    # assign bathymetry to a linear function
+    x, y = SpatialCoordinate(mesh2d)
+    depth_oce = 10.0
+    depth_riv = 5.0
+    bathymetry_2d.interpolate(depth_oce + (depth_riv - depth_oce)*x/lx)
+
+    # friction Manning coefficient
+    manning_2d = Function(P1_2d, name='Manning coefficient')
+    manning_low = 1e-3
+    manning_high = 1.5e-2
+    manning_2d.interpolate(
+        conditional(x < 60e3, manning_low, manning_high))
+    File('manning.pvd').write(manning_2d)
+
+    # create solver
+    solver_obj = solver2d.FlowSolver2d(mesh2d, bathymetry_2d)
+    options = solver_obj.options
+    options.manning_drag_coefficient = manning_2d
+    options.simulation_export_time = t_export
+    options.simulation_end_time = t_end
+    options.horizontal_velocity_scale = u_mag
+    options.output_directory = 'outputs_forward'
+    options.fields_to_export = ['uv_2d', 'elev_2d']
+    options.swe_timestepper_type = 'CrankNicolson'
+    if not hasattr(options.swe_timestepper_options, 'use_automatic_timestep'):
+        options.timestep = dt
+    options.update(model_options)
+
+    solver_obj.add_new_field(manning_2d, 'manning_2d',
+                             manning_2d.name(),
+                             'Manning2d', unit='s m-1/3')
+
+    solver_obj.create_equations()
+
+    if store_station_time_series:
+        # store elevation time series at stations
+        stations = [
+            ('stationA', (30e3, ly/2)),
+            ('stationB', (80e3, ly/2)),
+            ('stationC', (10e3, ly/2)),
+            ('stationD', (60e3, ly/2)),
+            ('stationE', (96e3, ly/2)),
+        ]
+        for name, (sta_x, sta_y) in stations:
+            cb = TimeSeriesCallback2D(
+                solver_obj, ['elev_2d'], sta_x, sta_y, name,
+                append_to_log=False
+            )
+            solver_obj.add_callback(cb)
+
+    # set initial condition for elevation, piecewise linear function
+    elev_init_2d = Function(P1_2d, name='Initial elevation')
+    elev_height = 6.0
+    elev_ramp_lx = 80e3
+    elev_init_2d.interpolate(
+        conditional(x < elev_ramp_lx, elev_height*(1 - x/elev_ramp_lx), 0.0))
+
+    solver_obj.add_new_field(elev_init_2d, 'elev_init_2d',
+                             elev_init_2d.name(),
+                             'InitialElev2d', unit='m')
+
+    return solver_obj
