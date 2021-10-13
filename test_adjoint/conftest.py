f5c8923f2ef9fc6d702af93ac1471224d706b50c
Stephan Kramer <s.kramer@imperial.ac.uk>
2021-10-13 11:56:56 +0100

    This class has moved (#275)

--- test_adjoint/conftest.py
+++ test_adjoint/conftest.py
@@ -4,7 +4,7 @@ def pytest_runtest_teardown(item, nextitem):
     """Clear Thetis caches after running a test"""
     from firedrake.tsfc_interface import TSFCKernel
     from pyop2.op2 import Kernel
-    from pyop2.base import JITModule
+    from pyop2.parloop import JITModule
     from pyadjoint import get_working_tape
 
     # disgusting hack, clear the Class-Cached objects in PyOP2 and
