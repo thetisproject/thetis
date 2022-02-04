1ef8d334f45ae1263330e0559df9bdcead083873
Connor Ward <c.ward20@imperial.ac.uk>
2022-02-04 16:30:30 +0000

    Fix for renamed PyOP2 objects

--- test_adjoint/conftest.py
+++ test_adjoint/conftest.py
@@ -3,15 +3,13 @@
 def pytest_runtest_teardown(item, nextitem):
     """Clear Thetis caches after running a test"""
     from firedrake.tsfc_interface import TSFCKernel
-    from pyop2.op2 import Kernel
-    from pyop2.parloop import JITModule
+    from pyop2.global_kernel import GlobalKernel
     from pyadjoint import get_working_tape
 
     # disgusting hack, clear the Class-Cached objects in PyOP2 and
     # Firedrake, otherwise these will never be collected.  The Kernels
     # get very big with bendy on.
-    Kernel._cache.clear()
     TSFCKernel._cache.clear()
-    JITModule._cache.clear()
+    GlobalKernel._cache.clear()
     # clear the adjoint tape, so subsequent tests don't interfere
     get_working_tape().clear_tape()
