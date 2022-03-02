0762a35d53c5f6012a429b641aeb6f97d36ff3a2
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2022-03-02 11:31:16 +0000

    north_sea: run script

---
+++ examples/north_sea/run.py
@@ -0,0 +1,16 @@
+from thetis import *
+import time as time_mod
+from model_config import *
+
+
+solver_obj, start_time, update_forcings = construct_solver(
+    start_date=datetime.datetime(2022, 1, 15, tzinfo=sim_tz),
+    end_date=datetime.datetime(2022, 1, 18, tzinfo=sim_tz),
+)
+print_output(f"Exporting to {solver_obj.options.output_directory}")
+solver_obj.load_state(14, outputdir="outputs_spinup", t=0, iteration=0)
+update_forcings(0.0)
+tic = time_mod.perf_counter()
+solver_obj.iterate(update_forcings=update_forcings)
+toc = time_mod.perf_counter()
+print_output(f"Total duration: {toc-tic:.2f} seconds")
