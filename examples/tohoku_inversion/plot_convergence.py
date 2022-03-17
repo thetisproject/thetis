2780fb69ea53bcb00a10fb3c1c7dd619cf75db04
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2022-03-17 13:29:41 +0000

    tohoku: plot formatting

--- examples/tohoku_inversion/plot_convergence.py
+++ examples/tohoku_inversion/plot_convergence.py
@@ -3,6 +3,18 @@ import matplotlib.pyplot as plt
 import numpy
 
 
+colours = {
+    "CG1": "C0",
+    "DG0": "C1",
+    "DG1": "C2",
+    "box": "C3",
+    "radial": "C4",
+    "okada": "C5",
+}
+markers = ["x", "+", "^", "v", "o", "h"]
+lines = ["-", "--", ":"] * 2
+counts = {approach: 0 for approach in colours}
+
 fig, axes = plt.subplots()
 inv_dir = "outputs_elev-init-optimization"
 fpaths = glob.glob(f"{inv_dir}_*")
@@ -10,13 +22,24 @@ if len(fpaths) == 0:
     raise ValueError("Nothing to plot!")
 for fpath in fpaths:
     source_model = fpath.split(inv_dir + "_")[-1]
+    colour = "k"
+    marker = ""
+    line = "-"
+    kw = {"label": source_model}
+    for approach, c in colours.items():
+        if source_model.startswith(approach):
+            kw["color"] = c
+            kw["marker"] = markers[counts[approach]]
+            kw["linestyle"] = lines[counts[approach]]
+            counts[approach] += 1
+            break
     J_progress = numpy.load(f"{fpath}/J_progress.npy")
     it = numpy.arange(1, len(J_progress) + 1)
-    axes.loglog(it, J_progress, label=source_model)
+    axes.loglog(it, J_progress, markevery=5, **kw)
 axes.set_xlabel("Iteration")
 axes.set_ylabel("Mean square error")
-axes.grid(True)
-axes.legend()
+axes.grid(True, which="both")
+axes.legend(ncol=2)
 imgfile = "optimization_progress.png"
 print(f"Saving to {imgfile}")
 plt.savefig(imgfile, dpi=200, bbox_inches="tight")
