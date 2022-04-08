3cb4173673e0c75dcf5bcd95ddf8ede416cb7995
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2022-04-08 19:01:35 +0100

    tohoku: use InversionManager

--- examples/tohoku_inversion/inverse_problem.py
+++ examples/tohoku_inversion/inverse_problem.py
@@ -44,25 +44,13 @@ options = solver_obj.options
 if not options.no_exports:
     print_output(f"Exporting to {options.output_directory}")
 
-# Choose optimisation parameters
+# Setup controls
 control = "elev_init"
 nc = len(controls)
 if nc == 1:
     control_bounds = [-numpy.inf, numpy.inf]
 else:
     control_bounds = [[-numpy.inf] * nc, [numpy.inf] * nc]
-op = inversion_tools.OptimisationProgress(
-    options.output_directory,
-    real=source_model[:2] not in ("CG", "DG"),
-    no_exports=options.no_exports,
-)
-for c in controls:
-    op.add_control(c)
-
-# Define the (appropriately scaled) cost function
-dt_const = Constant(solver_obj.dt)
-total_time_const = Constant(options.simulation_end_time)
-J_scalar = dt_const / total_time_const
 
 # Set up observation and regularization managers
 observation_data_dir = f"{pwd}/observations"
@@ -70,66 +58,43 @@ variable = "elev"
 station_names = list(stations.keys())
 start_times = [dat["interval"][0] for sta, dat in stations.items()]
 end_times = [dat["interval"][1] for sta, dat in stations.items()]
-stationmanager = inversion_tools.StationObservationManager(
-    mesh2d, J_scalar=J_scalar, output_directory=options.output_directory
+sta_manager = inversion_tools.StationObservationManager(
+    mesh2d, output_directory=options.output_directory
 )
-stationmanager.load_observation_data(
+sta_manager.load_observation_data(
     observation_data_dir,
     station_names,
     variable,
     start_times=start_times,
     end_times=end_times,
 )
-stationmanager.set_model_field(solver_obj.fields.elev_2d)
+sta_manager.set_model_field(solver_obj.fields.elev_2d)
 
-# Compute regularization term
+# Set regularization parameter
+gamma_hessian_list = []
 if source_model[:2] in ("CG", "DG"):
-    gamma_hessian_list = [Constant(0.1)]
-    reg_manager = inversion_tools.ControlRegularizationManager(
-        op.control_coeff_list,
-        gamma_hessian_list,
-        J_scalar=J_scalar,
-    )
-    op.J = reg_manager.eval_cost_function()
-else:
-    reg_manager = None
+    gamma_hessian_list.append(Constant(0.1))
+
+# Define the scaling for the cost function so that J ~ O(1)
+J_scalar = Constant(solver_obj.dt / options.simulation_end_time)
+
+# Create inversion manager and add controls
+inv_manager = inversion_tools.InversionManager(
+    sta_manager, real=source_model[:2] not in ("CG", "DG"),
+    output_dir=options.output_directory, no_exports=options.no_exports,
+    penalty_parameters=gamma_hessian_list, cost_function_scaling=J_scalar,
+    test_consistency=do_consistency_test, test_gradient=do_taylor_test)
+for c in controls:
+    inv_manager.add_control(c)
 
 # Extract the regularized cost function
-cost_function = inversion_tools.get_cost_function(
-    solver_obj, op, stationmanager, reg_manager=reg_manager, weight_by_variance=True
-)
+cost_function = inv_manager.get_cost_function(solver_obj)
 
 # Solve and setup the reduced functional
-solver_obj.iterate(export_func=cost_function)
-Jhat = ReducedFunctional(op.J, op.control_list, **op.rf_kwargs)
-pause_annotation()
-
-if do_consistency_test:
-    print_output("Running consistency test")
-    J = Jhat(op.control_coeff_list)
-    assert numpy.isclose(J, op.J)
-    print_output("Consistency test passed!")
-
-if do_taylor_test:
-    func_list = []
-    for f in op.control_coeff_list:
-        dc = Function(f.function_space()).assign(f)
-        func_list.append(dc)
-    minconv = taylor_test(Jhat, op.control_coeff_list, func_list)
-    assert minconv > 1.9
-    print_output("Taylor test passed!")
-
-
-def optimisation_callback(m):
-    """
-    Stash optimisation progress after successful line search.
-    """
-    op.update_progress()
-    stationmanager.dump_time_series()
-
+solver_obj.iterate(update_forcings=cost_function)
+inv_manager.stop_annotating()
 
 # Run inversion
-opt_method = "L-BFGS-B"
 opt_verbose = -1
 opt_options = {
     "maxiter": args.maxiter,
@@ -138,30 +103,20 @@ opt_options = {
 }
 if os.getenv("THETIS_REGRESSION_TEST") is not None:
     opt_options["maxiter"] = 1
-print_output(f"Running {opt_method} optimisation")
-op.reset_counters()
-op.start_clock()
-J = float(Jhat(op.control_coeff_list))
-op.set_initial_state(J, Jhat.derivative(), op.control_coeff_list)
-control_opt_list = minimize(
-    Jhat,
-    method=opt_method,
-    bounds=control_bounds,
-    callback=optimisation_callback,
-    options=opt_options,
-)
+control_opt_list = inv_manager.minimize(
+    opt_method="L-BFGS-B", bounds=control_bounds, **opt_options)
 if options.no_exports:
     sys.exit(0)
 if source_model[:2] in ("CG", "DG"):
     cc = control_opt_list
     if not isinstance(control_opt_list, Function):
         cc = cc[0]
-    oc = op.control_coeff_list[0]
+    oc = inv_manager.control_coeff_list[0]
     name = cc.name()
     oc.rename(name)
 else:
     oc = initial_condition(
-        mesh2d, source_model=source_model, controls=op.control_coeff_list
+        mesh2d, source_model=source_model, controls=inv_manager.control_coeff_list
     )[0]
 outfile = File(f"{options.output_directory}/elevation_optimised.pvd")
 print_function_value_range(oc, prefix="Optimal")
