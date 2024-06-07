576f2a0673be45a1b0d351b03ff9a3b4756115e1
Connor Jordan <C.Jordan@ed.ac.uk>
2024-06-07 16:46:37 +0100

    Import VTKFile at top level

--- examples/channel_inversion/inverse_problem.py
+++ examples/channel_inversion/inverse_problem.py
@@ -1,6 +1,6 @@
 from thetis import *
+import firedrake as fd
 from firedrake.adjoint import *
-from firedrake.output.vtk_output import VTKFile
 import numpy
 import thetis.inversion_tools as inversion_tools
 from model_config import construct_solver
@@ -134,4 +134,4 @@ for oc, cc in zip(control_opt_list, inv_manager.control_coeff_list):
     oc.rename(name)
     print_function_value_range(oc, prefix='Optimal')
     if not no_exports:
-        VTKFile(f'{options.output_directory}/{name}_optimised.pvd').write(oc)
+        fd.VTKFile(f'{options.output_directory}/{name}_optimised.pvd').write(oc)
