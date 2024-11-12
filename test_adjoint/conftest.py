8c6f9d1ed954a05a8d1013f79ab5b7a702c9a4a2
Connor Jordan <115144692+cpjordan@users.noreply.github.com>
2024-11-12 16:29:56 +0000

    Remove pytest cache clearing (#375)
    Cache clearing no longer needed with recent Firedrake/PyOP2 updates

--- test_adjoint/conftest.py
+++ test_adjoint/conftest.py
@@ -1,15 +1,8 @@
 
 
 def pytest_runtest_teardown(item, nextitem):
-    """Clear Thetis caches after running a test"""
-    from firedrake.tsfc_interface import TSFCKernel
-    from pyop2.global_kernel import GlobalKernel
+    """Clear adjoint tape after running a test"""
     from pyadjoint import get_working_tape
 
-    # disgusting hack, clear the Class-Cached objects in PyOP2 and
-    # Firedrake, otherwise these will never be collected.  The Kernels
-    # get very big with bendy on.
-    TSFCKernel._cache.clear()
-    GlobalKernel._cache.clear()
     # clear the adjoint tape, so subsequent tests don't interfere
     get_working_tape().clear_tape()
