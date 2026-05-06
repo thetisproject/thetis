65031cacfda9ff36eba370dbe9c9287a64e3fd51
Connor Jordan <C.Jordan@ed.ac.uk>
2026-05-06 15:55:08 +0100

    Split parallel tests

--- test_adjoint/conftest.py
+++ test_adjoint/conftest.py
@@ -2,8 +2,9 @@
 
 def pytest_runtest_teardown(item, nextitem):
     """Clear adjoint tape after running a test"""
-    from pyadjoint import pause_annotation, get_working_tape
+    from pyadjoint import get_working_tape
 
-    # pause and then clear the adjoint tape, so subsequent tests don't interfere
-    pause_annotation()
-    get_working_tape().clear_tape()
+    # clear the adjoint tape, so subsequent tests don't interfere
+    tape = get_working_tape()
+    if tape is not None:
+        tape.clear_tape()
